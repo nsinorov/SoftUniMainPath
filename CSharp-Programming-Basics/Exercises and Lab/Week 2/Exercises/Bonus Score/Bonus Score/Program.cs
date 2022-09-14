a40c406c2559b85347c120274da21384e7ab8fcb
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-15 00:46:42 +0300

    Add files via upload

---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 2/Exercises/Bonus Score/Bonus Score/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Bonus_Score
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            double bonus = 0.0;
+
+            if( num <= 100)
+            {
+                bonus = 5;
+            }
+            else if ( num > 1000)
+            {
+                bonus = num * 0.1;
+            }
+            else
+            {
+                bonus = num * 0.2;
+            }
+
+            if (num % 2 == 0)
+            {
+                bonus += 1;
+            }
+            else if ( num % 5 == 0)
+            {
+                bonus += 2; 
+            }
+            Console.WriteLine(bonus);
+            Console.WriteLine(bonus + num);
+        }
+    }
+}
---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 2/Exercises/Godzilla vs. Kong/Godzilla vs. Kong/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Godzilla_vs._Kong
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {       
+            double budged = double.Parse(Console.ReadLine());
+            int numOfStatists = int.Parse(Console.ReadLine());
+            double priceForOutfit = double.Parse(Console.ReadLine());
+
+            double decor = budged * 0.1;
+
+            if (numOfStatists >= 150)
+            {
+                priceForOutfit = priceForOutfit - (priceForOutfit *  0.1);
+            }
+
+            double sumForOutfit = numOfStatists * priceForOutfit;
+
+            double totalPrice = decor + sumForOutfit;
+            double totalLeft = budged - totalPrice;
+
+            if (totalPrice > budged)
+            {
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {Math.Abs(totalLeft):F2} leva more.");
+            }
+            else
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {totalLeft:F2} leva left.");
+            }	    
+        }
+    }
+}
