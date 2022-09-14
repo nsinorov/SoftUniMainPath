99e430c15bc2a1c737fb671084779977217730fd
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-14 23:19:27 +0300

    Add files via upload

---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 1/FirstWeek Lectures/Pet Shop/Pet Shop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Pet_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double singleDogFood = 2.50;
+            double singleCatFood = 4;
+            int foodQuantityForDogs = int.Parse(Console.ReadLine());
+            int foodQuantityForCats = int.Parse(Console.ReadLine());
+
+            double totalPrice = (foodQuantityForDogs * singleDogFood)+ (foodQuantityForCats * singleCatFood);
+
+            Console.WriteLine($"{totalPrice} lv.");
+        }
+    }
+}
