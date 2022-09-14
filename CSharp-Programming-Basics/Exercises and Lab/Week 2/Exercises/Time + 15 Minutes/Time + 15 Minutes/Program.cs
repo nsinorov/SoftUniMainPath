fd37fcd807fefcefbc31c81cbb11b2a31a93d08d
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-15 00:42:57 +0300

    Add files via upload

---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 2/Exercises/Time + 15 Minutes/Time + 15 Minutes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Time___15_Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int  min = int.Parse(Console.ReadLine());
+
+            min += (hours * 60);
+
+            int minAfter15Min = min + 15;
+
+            int hAfter = minAfter15Min / 60;
+            int minAfter = minAfter15Min % 60;
+
+            if (hAfter > 23)
+            {
+                hAfter = 0;
+            }
+
+            if ( minAfter < 10)
+            {
+                Console.WriteLine($"{hAfter}:0{minAfter}");
+            }
+            else
+            {
+                Console.WriteLine($"{hAfter}:{minAfter}");
+            }
+           
+        }
+    }
+}
+
---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 2/Exercises/Toy Shop/Toy Shop/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Toy_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {          
+            double priceForVacation = double.Parse(Console.ReadLine());
+            int numPuzzles = int.Parse(Console.ReadLine());
+            int numDolls = int.Parse(Console.ReadLine());
+            int numBears = int.Parse(Console.ReadLine());
+            int numMinions = int.Parse(Console.ReadLine());
+            int numTrucks = int.Parse(Console.ReadLine());
+
+            double puzzlePrice = numPuzzles *2.60;
+            int priceDoll = numDolls * 3;
+            double priceBear = numBears * 4.10;
+            double priceMinion = numMinions * 8.20;
+            int priceTruck = numTrucks * 2;
+
+            double totalQuantity = numPuzzles + numDolls + numBears + numMinions + numTrucks;
+
+            double priceSum = puzzlePrice + priceDoll + priceBear + priceMinion + priceTruck;
+            
+            if (totalQuantity >= 50)
+            {
+                priceSum -= priceSum * 0.25;
+            }
+                priceSum -= priceSum * 0.1;
+
+            if (priceSum >= priceForVacation)
+            {
+                priceSum -= priceForVacation;
+                Console.WriteLine($"Yes! {priceSum:f2} lv left.");
+            }
+            else
+            {
+                priceForVacation -= priceSum;
+                Console.WriteLine($"Not enough money! {priceForVacation:f2} lv needed.");
+            }
+        }
+    }
+}
