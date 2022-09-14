6c237dd49e0f8745e5ee8c33036ac7988e57133b
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-14 18:11:07 +0300

    Add files via upload

---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 1/FirstWeek Exercises/Fish Tank/Fish Tank/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Fish_Tank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {       
+            int lenght = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine()); 
+            int height = int.Parse(Console.ReadLine());
+            double percentage = double.Parse(Console.ReadLine());
+
+            //изчисления
+             int volumeAquarium = lenght * width * height; //см^3 
+             double volumeLitters = volumeAquarium * 0.001;
+
+            percentage = percentage * 0.01;
+
+            double neededLitters = volumeLitters * (1 - percentage);
+
+            Console.Write(neededLitters);
+        }
+    }
+}
