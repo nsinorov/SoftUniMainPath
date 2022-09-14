99f5a8379d3e31a4c680df40261be566f9892e5d
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-14 18:11:46 +0300

    Add files via upload

---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 1/FirstWeek Exercises/Food Delivery/Food Delivery/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Food_Delivery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //четем какво ни е дадено
+            double menuChicken = 10.35;
+            double menuFish = 12.40;
+            double menuVegeterian = 8.15;
+            double delivery = 2.50;
+            //вход
+            double quantitiChicken = double.Parse(Console.ReadLine());
+            double quantitiFish = double.Parse(Console.ReadLine());
+            double quantitiVegeterian = double.Parse(Console.ReadLine());
+
+            double priceChicken = quantitiChicken * menuChicken;
+            double priceFish = quantitiFish * menuFish;
+            double priceVegeterian = quantitiVegeterian * menuVegeterian;
+            //изчисления
+            double sumPrice = priceChicken + priceFish + priceVegeterian;
+
+            double dessert = sumPrice * 0.2;
+
+            double totalPrice = sumPrice + dessert + delivery;
+            //печат
+            Console.Write(totalPrice);
+        }
+    }
+}
