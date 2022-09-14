396430744f2fa822b1eae8a2bb5acb6044ce36b9
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-15 00:48:04 +0300

    Add files via upload

---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 2/Exercises/World-Swimming-Record/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace World_Swimming_Record
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {    
+            double recordInSeconds = double.Parse(Console.ReadLine());
+            double distanceInMeters = double.Parse(Console.ReadLine());
+            double timeInSec = double.Parse(Console.ReadLine());
+
+            double neededDistance = distanceInMeters * timeInSec;
+
+            double every15Meters = (Math.Floor(distanceInMeters / 15)) * 12.5;
+
+            double sumTime = Math.Abs(neededDistance + every15Meters);
+
+            if (recordInSeconds > sumTime)
+            {
+                double toralNewRecord = neededDistance + every15Meters;
+                Console.WriteLine($"Yes, he succeeded! The new world record is {toralNewRecord:F2} seconds.");             
+            }
+            else if (recordInSeconds <= sumTime)
+            {
+                double totalTimeSlower = sumTime - recordInSeconds;
+                Console.WriteLine($"No, he failed! He was {totalTimeSlower:F2} seconds slower.");
+            }
+        }
+    }
+}
