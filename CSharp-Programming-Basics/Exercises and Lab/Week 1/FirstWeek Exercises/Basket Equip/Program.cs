2c144a33e57ea1dc81c8e13e9cf23d7b21e9e341
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-14 18:08:38 +0300

    Add files via upload

---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 1/FirstWeek Exercises/Basket Equip/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Basketball_Equipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int priceForOneYear = int.Parse(Console.ReadLine());
+
+            double priceSneakers = priceForOneYear - priceForOneYear * 0.4;
+            double priceOutfit = priceSneakers - priceSneakers * 0.2;
+            double ball = priceOutfit / 4;
+            double accessories = ball / 5;
+
+            double totalExpenses = priceForOneYear + priceSneakers + priceOutfit + ball + accessories;
+
+            Console.Write(totalExpenses);
+
+        }
+    }
+}
