99669d4dc08ba4983d6b30bc1c256127a74698dd
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-14 23:16:10 +0300

    Add files via upload

---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 1/FirstWeek Lectures/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Inches_to_Centimeters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          double num = double.Parse(Console.ReadLine());
+            double inches = 2.54;
+            double centemeters = num * inches;
+            Console.WriteLine(centemeters);
+        }
+    }
+}
