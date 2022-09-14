9460967f29079edaa351a3486c7447b99312f747
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-14 18:03:39 +0300

    Add files via upload

---
+++ CSharp-Programming-Basics/Exam/Unique PIN Codes/Unique PIN Codes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Unique_PIN_Codes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // vhod
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            for (int l = 2; l <= num1; num1 += 2)
+            {
+                for (int k = 2; k <= num2; num2++)
+                {
+                    for (int j = 2; j <= num3; num3 += 2)
+                    {
+                       if(num2 == 2 || num2 == 3 || num2 == 5 || num2 == 7)
+                        {
+                            Console.Write($"{l} {k} {j} ");
+                        }
+                                   
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ CSharp-Programming-Basics/Exam/Workout/Workout/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Workout
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            //vhod 
+
+            int nQuantityOfDays = int.Parse(Console.ReadLine());
+            double mKmForOneDay = double.Parse(Console.ReadLine());
+
+            double progress = mKmForOneDay;
+
+            //for and logic
+
+            for (int i = 0; i < nQuantityOfDays; i++)
+            {
+                double nextDay = double.Parse(Console.ReadLine());
+                mKmForOneDay = mKmForOneDay + mKmForOneDay * (nextDay / 100);
+                progress += mKmForOneDay;
+            }
+            if(progress >= 1000)
+            {
+                Console.WriteLine($"You've done a great job running {Math.Ceiling(progress - 1000)} more kilometers!");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry Mrs. Ivanova, you need to run {Math.Ceiling(1000 - progress)} more kilometers");
+            }
+        }
+    }
+}
