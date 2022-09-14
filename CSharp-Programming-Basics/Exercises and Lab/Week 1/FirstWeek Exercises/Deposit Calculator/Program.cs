f1c7b7ffb6c24472951dbfa67a67e668b00b77c9
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-14 18:10:06 +0300

    Add files via upload

---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 1/FirstWeek Exercises/Deposit Calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Deposit_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double deposit = double.Parse(Console.ReadLine());
+            double termOfDeposit = double.Parse(Console.ReadLine());    
+            double yearPercentage = double.Parse(Console.ReadLine());
+
+            double wholePercentage = deposit * yearPercentage;
+            double percentageMonth = wholePercentage / 12;
+
+            double sum = deposit + termOfDeposit * ((deposit * yearPercentage * 0.01)  / 12);
+
+            Console.Write(sum);
+        }
+    }
+}
