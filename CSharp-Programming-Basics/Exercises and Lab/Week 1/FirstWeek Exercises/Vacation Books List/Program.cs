ab3a5af80ef70766f6a5a3c28f33b44530cef66c
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-14 18:16:15 +0300

    Add files via upload

---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 1/FirstWeek Exercises/Vacation Books List/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Vacation_Books_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int numPages = int.Parse(Console.ReadLine());
+            int pagesForOneHour = int.Parse(Console.ReadLine());    
+            int days = int.Parse(Console.ReadLine());
+
+            int sumTime = numPages / pagesForOneHour;
+            int neededHours = sumTime / days;
+
+            Console.Write(neededHours);
+        }
+    }
+}
