9b13c52e6b803064025fa78e64d7913d1d6f2cc9
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-14 18:12:32 +0300

    Add files via upload

---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 1/FirstWeek Exercises/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Radians_to_Degrees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radians = double.Parse(Console.ReadLine());
+            double degrees = radians * 180 / Math.PI; 
+            Console.Write(degrees);
+        }
+    }
+}
