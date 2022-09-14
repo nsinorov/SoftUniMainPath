322f06a1ef2e527bc118fd2a8595550bfb9dc8da
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-14 18:04:57 +0300

    Add files via upload

---
+++ CSharp-Programming-Basics/Exam/Football Kit/Football Kit/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Football_Kit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Знае се, че цената на шортите е 75% от цената на тениските
+            //  а цената на чорапите е 20% от цената на шортите
+            // Бутонките струват два пъти колкото тениската и шортите взети заедно
+            // той има карта за отстъпка на стойност 15% от общата сума 
+
+            //  Ако сметката на Пепи е по-висока или равна на дадена сума, той получава подарък - точно копие на топката от световното
+
+            // Напишете програма, която изчислява дали Пепи е спечелил топката. 
+
+            //vhod
+            double priceOfOneShirt = double.Parse(Console.ReadLine()); // price of shirt 25
+            double budgedNeeded = double.Parse(Console.ReadLine());
+            
+            double priceOfShorts = priceOfOneShirt * 0.75; // --> цена на шортите
+            double priceOfSocks = priceOfShorts * 0.20; // --> цена на чорапите
+            double priceOfButonki = (priceOfOneShirt + priceOfShorts) * 2;
+
+            double totalSum = priceOfOneShirt + priceOfShorts + priceOfSocks + priceOfButonki;
+
+            double sumAfterDiscount = totalSum - (totalSum * 0.15);
+
+            if(sumAfterDiscount >= budgedNeeded)
+            {
+                Console.WriteLine("Yes, he will earn the world-cup replica ball!");
+                Console.WriteLine($"His sum is {sumAfterDiscount:F2} lv.");            
+            }
+            else
+            {
+                Console.WriteLine("No, he will not earn the world-cup replica ball.");
+                Console.WriteLine($"He needs {Math.Abs(sumAfterDiscount - budgedNeeded):F2} lv. more.");
+            }
+        }
+    }
+}
---
+++ CSharp-Programming-Basics/Exam/Hair Salon/Hair Salon/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Hair_Salon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //vhod
+            int targetForTheDay = int.Parse(Console.ReadLine());
+           double earnedMoney = 0;
+
+            string working = "";
+      
+            while (working != "closed" || earnedMoney >= targetForTheDay)
+            {
+                if (earnedMoney >= targetForTheDay)
+                {
+                    Console.WriteLine("You have reached your target for the day!");
+                    Console.WriteLine($"Earned money: {earnedMoney}lv.");
+                    break;
+                }
+                string typeOfService = Console.ReadLine();
+
+                if (typeOfService == "haircut")
+                {
+                    string type = Console.ReadLine();
+
+                    if (type == "ladies")
+                    {
+                        earnedMoney += 20;                                  
+                    }
+                   
+                    else if (type == "mens")
+                    {
+                        earnedMoney += 15;
+                       
+                    }             
+                    else if (type == "kids")
+                    {
+                        earnedMoney += 10;              
+                    }                
+                    continue;
+                }
+
+                else if (typeOfService == "color")
+                {
+                    string service = Console.ReadLine();
+
+                    if (service == "touch up")
+                    {
+                        earnedMoney += 20;
+                    }
+                    else if (service == "full color")
+                    {
+                        earnedMoney += 30;
+                    }                 
+                    continue;                 
+                }
+             
+                if (earnedMoney < targetForTheDay)
+                {
+                    Console.WriteLine($"Target not reached! You need {targetForTheDay - earnedMoney}lv. more.");
+                    Console.WriteLine($"Earned money: {earnedMoney}lv.");
+                    break;
+                }
+               
+            }        
+        }
+    }
+}
