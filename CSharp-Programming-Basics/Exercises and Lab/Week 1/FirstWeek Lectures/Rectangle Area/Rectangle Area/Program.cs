b524d3e4d4bde111214dbcedafd51ee4066a344f
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-14 23:18:32 +0300

    Add files via upload

---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 1/FirstWeek Lectures/Rectangle Area/Rectangle Area/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Rectangle_Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int a = int.Parse(Console.ReadLine());   
+           int b = int.Parse(Console.ReadLine());
+            int area = a * b;
+            Console.WriteLine(area);
+        }
+    }
+}
---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 1/FirstWeek Lectures/Yard Greening/Yard Greening/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Yard_Greening
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //, която изчислява необходимате сума, които Божидара ще трябва да заплати на фирмата 
+            // Цената на един кв. м. е 7.61 лв със ДДС
+            //нейният двор е доста голям, фирмата изпълнител предлага 18% отстъпка от крайната цена.
+            double priceForOneYard = 7.61;
+            double discountEnd = 0.18;
+            //1. Вход	Кв. метри, които ще бъдат озеленени – реално число в интервала [0.00 … 10000.00] double        
+            double yardGreening = double.Parse(Console.ReadLine());
+            //Логика
+            double wholeYardGreening = yardGreening * priceForOneYard;
+            double discountFromTotalSum = discountEnd * wholeYardGreening;
+            double calculateFinalPrice = wholeYardGreening - discountFromTotalSum; 
+             
+            Console.WriteLine($"The final price is: {calculateFinalPrice} lv.");
+            Console.Write($"The discount is: {discountFromTotalSum} lv.");          
+        }
+    }
+}
