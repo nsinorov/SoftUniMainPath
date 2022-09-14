423fa2697f46696906436b0523c63b16bb4a9a51
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-15 00:39:49 +0300

    Add files via upload

---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 2/Lectures/Password Guess/Password Guess/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Password_Guess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentPass = Console.ReadLine();
+
+            if (currentPass == "s3cr3t!P@ssw0rd")
+            {
+                Console.WriteLine("Welcome");
+            }
+            else  
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
