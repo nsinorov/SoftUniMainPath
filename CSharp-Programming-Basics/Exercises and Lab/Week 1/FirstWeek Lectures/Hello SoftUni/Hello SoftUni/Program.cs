9b4f79fd47811567a250d43b5a9402ea8fc80df8
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-14 23:13:05 +0300

    Add files via upload

---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 1/FirstWeek Lectures/Hello SoftUni/Hello SoftUni/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_SoftUni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SoftUni");
+        }
+    }
+}
