17bc325103ba3c3ad8bc9d5dfb9abd244419464f
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-14 18:15:28 +0300

    Add files via upload

---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 1/FirstWeek Exercises/USD to BGN/USD to BGN/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace USD_to_BGN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double usd = double.Parse(Console.ReadLine());
+
+            double bgn = usd * 1.94;
+
+            Console.Write(bgn);
+        }
+    }
+}
