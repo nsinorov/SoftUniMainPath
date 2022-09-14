9ff0e8d11f567645e970dac1aa038c949af2629f
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-14 23:20:39 +0300

    Add files via upload

---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 1/FirstWeek Lectures/Projects Creation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Projects_Creation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            string nameOfTheArchitect = Console.ReadLine();
+            int numOfProjecst = int.Parse(Console.ReadLine());
+            int oneProject = 3;
+            int timeForAllProjects = numOfProjecst * oneProject; 
+
+            Console.WriteLine($"The architect {nameOfTheArchitect} will need {timeForAllProjects} hours to complete {numOfProjecst} project/s.");
+        }
+    }
+}
