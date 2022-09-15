0d5c04ce3c9d7d5e4db7e0ea96d79571d413621b
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-16 01:11:37 +0300

    Add files via upload

---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 3/Lectures/Cinema Ticket/Cinema Ticket/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Cinema_Ticket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          string day = Console.ReadLine();
+           int priceForTicket = 0;
+            switch (day)
+            {
+                case "Monday":
+                    priceForTicket = 12;
+                    break;
+                case "Tuesday":
+                    priceForTicket = 12;
+                    break;
+                case "Friday":            //тарикатски/глупав номер ;)
+                    priceForTicket = 12; 
+                    break;
+                case "Thursday":
+                    priceForTicket = 14;
+                    break;
+                case "Wednesday":
+                    priceForTicket = 14;
+                    break;
+                case "Saturday":
+                    priceForTicket = 16;
+                    break;
+                case "Sunday":
+                    priceForTicket = 16;
+                    break;
+                    default:                 
+                    break;
+            }
+            Console.WriteLine(priceForTicket);
+        }
+    }
+}
---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 3/Lectures/Fruit or Vegetable/Fruit or Vegetable/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Fruit_or_Vegetable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            
+            switch (product)
+            {
+                case "banana":
+                case "apple":
+                case "kiwi":
+                case "cherry":
+                case "lemon":
+                case "grapes":
+                    Console.WriteLine("fruit");
+                    break;
+                case "tomato":
+                case "cucumber":
+                case "pepper":
+                case "carrot":
+                    Console.WriteLine("vegetable");
+                    break;                
+                default:
+                    Console.WriteLine("unknown");
+                    break;                            
+            }                                            
+        }
+    }
+}
