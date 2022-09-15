18616355634a20bd67071504c75a8b85e2a2c366
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-16 01:31:33 +0300

    Add files via upload

---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 5/Lectures/Sequence 2k+1/Sequence 2k+1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int k = 1;
+
+            while (k <= number)
+            {
+                Console.WriteLine(k);
+                k = k * 2 + 1;
+            }        
+        }
+    }
+}
---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 5/Lectures/Sum Numbers/Sum Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            while(sum < number)
+            {
+                int currentNumber = int.Parse((Console.ReadLine()));
+                sum += currentNumber;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
