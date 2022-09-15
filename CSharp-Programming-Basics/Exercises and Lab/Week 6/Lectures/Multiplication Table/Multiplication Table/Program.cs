56a4e3a57abfb116a4cf4a7323e7c5918ad81cb1
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-16 01:38:21 +0300

    Add files via upload

---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 6/Lectures/Multiplication Table/Multiplication Table/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Multiplication_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int firstNum = 1; firstNum <= 10; firstNum++)
+            {
+                for (int secondNum = 1; secondNum <= 10; secondNum++)
+                {
+                    Console.WriteLine($"{firstNum} * {secondNum} = {firstNum * secondNum}");
+                }
+            }
+
+        }
+    }
+}
---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 6/Lectures/Sum of Two Numbers/Sum of Two Numbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Sum_of_Two_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            int startingNum = int.Parse(Console.ReadLine());
+            int endNum = int.Parse(Console.ReadLine());
+            int magicNum = int.Parse(Console.ReadLine());
+
+            int combination = 0;
+            bool isFound = false;
+
+            for(int firstNum = startingNum; firstNum <= endNum; firstNum++)
+            {
+                for(int secondNum = startingNum; secondNum <= endNum; secondNum++)
+                {
+                    combination++;
+                    if(firstNum + secondNum == magicNum)
+                    {
+                        isFound = true;
+                        Console.WriteLine($"Combination N:{combination} ({firstNum} + {secondNum} = {magicNum})");
+                        break;
+                    }
+                }
+                if (isFound)
+                {
+                    break;
+                }
+            }
+            if (!isFound)
+            {
+                Console.WriteLine($"{combination} combinations - neither equals {magicNum}");               
+            }
+        }
+    }
+}
