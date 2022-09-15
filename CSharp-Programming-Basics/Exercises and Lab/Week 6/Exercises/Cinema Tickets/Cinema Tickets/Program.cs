933a8fa9c01c30b7297af7d1c800819cad6bbd30
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-16 01:36:27 +0300

    Add files via upload

---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 6/Exercises/Cinema Tickets/Cinema Tickets/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Cinema_Tickets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int studentTickets = 0;
+            int standardTickets = 0;
+            int kidTickets = 0;
+
+            string input = Console.ReadLine();
+
+            while(input != "Finish")
+            {
+                int freeSpace = int.Parse(Console.ReadLine());
+                int tickets = 0;
+
+                string ticketType = Console.ReadLine();
+
+                while (ticketType != "End")
+                {
+                    tickets++;
+
+                    if (ticketType == "student")
+                    {
+                        studentTickets++;
+                    }
+                    else if(ticketType == "standard")
+                    {
+                        standardTickets++;
+                    }
+                    else
+                    {
+                        kidTickets++;
+                    }
+                   if(freeSpace == tickets)
+                    {
+                        break;
+                    }
+                    ticketType = Console.ReadLine();
+                   
+                }
+
+                double occypancy = tickets * 100.0 / freeSpace;
+                Console.WriteLine($"{input} - {occypancy:F2}% full.");
+                input = Console.ReadLine();
+
+            }
+            int totalTickets = studentTickets + standardTickets + kidTickets;
+
+            Console.WriteLine($"Total tickets: {totalTickets}");
+            Console.WriteLine($"{studentTickets * 100.0 / totalTickets:F2}% student tickets.");
+            Console.WriteLine($"{standardTickets * 100.0 / totalTickets:F2}% standard tickets.");
+            Console.WriteLine($"{kidTickets * 100.0 / totalTickets:F2}% kids tickets.");
+        }
+    }
+}
---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 6/Exercises/Number Pyramid/Number Pyramid/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Number_Pyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = int.Parse(Console.ReadLine());
+            int currentNum = 1;
+          
+            for(int i = 1; i <=num; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(currentNum+ " ");      
+                    currentNum++;
+                    if (currentNum > num)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine();
+                if (currentNum > num)
+                {
+                    break;
+                }           
+            }
+        }
+    }
+}
