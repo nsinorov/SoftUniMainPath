016c66f77f4f82b28e1b46c805b518748b47545f
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-16 01:23:07 +0300

    Add files via upload

---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 4/Lectures/Numbers N1/Numbers N.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Numbers_from_1_to_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = int.Parse(Console.ReadLine());
+
+           for(int i = num; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
