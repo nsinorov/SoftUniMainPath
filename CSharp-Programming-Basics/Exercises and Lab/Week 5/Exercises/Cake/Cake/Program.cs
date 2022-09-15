f3764235fc05eeba9c535364fd4f1b771554d8f1
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-16 01:30:12 +0300

    Add files via upload

---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 5/Exercises/Cake/Cake/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Cake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int w = int.Parse(Console.ReadLine());
+            int h = int.Parse(Console.ReadLine());
+
+            int cakeSize = w * h;
+            int cakePieces = 0;
+            string input = Console.ReadLine();
+
+
+            while(input != "STOP")
+            {
+                cakePieces+= int.Parse(input);
+
+                if(cakePieces > cakeSize)
+                {
+                    break;
+                }
+                input = Console.ReadLine();
+
+            }
+            int diff = cakeSize - cakePieces;
+
+            if(diff >= 0)
+            {
+                Console.WriteLine($"{diff} pieces are left.");
+            }
+
+            else
+            {
+                Console.WriteLine($"No more cake left! You need {Math.Abs(diff)} pieces more.");
+            }
+        }
+    }
+}
