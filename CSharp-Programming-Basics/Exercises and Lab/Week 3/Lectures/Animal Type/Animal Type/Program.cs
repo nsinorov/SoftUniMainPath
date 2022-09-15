03705df66db9cdc72e28bdf8f558121c1ca5ab25
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-16 01:12:03 +0300

    Add files via upload

---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 3/Lectures/Animal Type/Animal Type/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Animal_Type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string animalType = Console.ReadLine();
+
+            switch (animalType)
+            {
+                case "dog":
+                    Console.WriteLine("mammal");
+                    break;
+                case "crocodile":
+                case "tortoise":
+                case "snake":
+                    Console.WriteLine("reptile");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
