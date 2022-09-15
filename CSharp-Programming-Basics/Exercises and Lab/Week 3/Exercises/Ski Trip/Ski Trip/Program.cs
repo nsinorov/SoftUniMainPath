03a38a9bb7e97853a75ca119cb9e38ecb3ec61f1
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-16 01:02:55 +0300

    Add files via upload

---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 3/Exercises/Ski Trip/Ski Trip/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Ski_Trip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int daysStaying = int.Parse(Console.ReadLine());
+            string typeOfRoom = Console.ReadLine(); 
+            string score = Console.ReadLine();
+            daysStaying = daysStaying - 1;
+            double midSum = 0;
+            double totalSum = 0;
+    
+          if(typeOfRoom == "room for one person" && daysStaying < 10)
+            {
+                midSum = daysStaying * 18.00;            
+            }
+
+          else if(typeOfRoom == "room for one person" && daysStaying < 15)
+            {
+                midSum = daysStaying * 18.00;            
+            }
+
+          else if(typeOfRoom == "room for one person" && daysStaying > 15)
+            {
+                midSum = daysStaying * 18.00;         
+            }
+
+            if (typeOfRoom == "apartment" && daysStaying < 10)
+            {
+                midSum = (daysStaying * 25.00);
+                midSum = midSum - (midSum * 0.3);
+            }
+
+          else if(typeOfRoom == "apartment" && daysStaying < 15)
+            {
+                midSum = (daysStaying * 25.00);
+                midSum = midSum - (midSum * 0.35);           
+            }
+
+          else if(typeOfRoom == "apartment" && daysStaying > 15)
+            {
+                midSum = (daysStaying * 25.00);
+                midSum = midSum - (midSum * 0.5);
+            }
+
+          if(typeOfRoom == "president apartment" && daysStaying < 10)
+            {
+                midSum = (daysStaying * 35.00);
+                midSum = midSum - (midSum * 0.1);
+            }
+
+          else if(typeOfRoom == "president apartment" && daysStaying < 15)
+            {
+                midSum = (daysStaying * 35.00);
+                midSum = midSum - (midSum * 0.15);
+            }
+
+            else if (typeOfRoom == "president apartment" && daysStaying > 15)
+            {
+                midSum = (daysStaying * 35.00);
+                midSum = midSum - (midSum * 0.2);
+            }
+
+          if(score == "positive")
+            {
+                totalSum = midSum + (midSum * 0.25);
+                Console.WriteLine($"{totalSum:F2}");
+            }
+
+            else if(score == "negative")
+            {
+                totalSum = midSum - (midSum * 0.1);
+                Console.WriteLine($"{totalSum:F2}");
+            }
+        }
+    }
+}
---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 3/Exercises/Summer Outfit/Summer Outfit/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Summer_Outfit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int celsius = int. Parse(Console.ReadLine());
+           string timeOfDay = Console.ReadLine();
+
+            if (celsius >= 10 && celsius <= 18)
+            {
+                if (timeOfDay == "Morning")
+                {
+                    string outfit = "Sweatshirt";
+                    string shoes = "Sneakers";
+                    Console.WriteLine($"It's {celsius} degrees, get your {outfit} and {shoes}.");
+                }
+                else if (timeOfDay == "Afternoon" || timeOfDay == "Evening")
+                {
+                    string outfit = "Shirt";
+                    string shoes = "Moccasins";
+                    Console.WriteLine($"It's {celsius} degrees, get your {outfit} and {shoes}.");
+                }
+            }
+                if(celsius > 18 && celsius <= 24)
+                {
+                    if(timeOfDay == "Afternoon")
+                    {
+                        string outfit = "T-Shirt";
+                        string shoes = "Sandals";
+                        Console.WriteLine($"It's {celsius} degrees, get your {outfit} and {shoes}.");
+                    }
+                    else if(timeOfDay == "Morning" || timeOfDay == "Evening")
+                    {
+                        string outfit = "Shirt";
+                        string shoes = "Moccasins";
+                        Console.WriteLine($"It's {celsius} degrees, get your {outfit} and {shoes}.");
+                    }
+                }
+
+                if(celsius >= 25)
+                {
+                    if(timeOfDay == "Morning")
+                    {
+                        string outfit = "T-Shirt";
+                        string shoes = "Sandals";
+                        Console.WriteLine($"It's {celsius} degrees, get your {outfit} and {shoes}.");
+                    }
+                    else if(timeOfDay == "Afternoon")
+                    {
+                        string outfit = "Swim Suit";
+                        string shoes = "Barefoot";
+                        Console.WriteLine($"It's {celsius} degrees, get your {outfit} and {shoes}.");
+                    }
+                    else if(timeOfDay == "Evening")
+                    {
+                        string outfit = "Shirt";
+                        string shoes = "Moccasins";
+                        Console.WriteLine($"It's {celsius} degrees, get your {outfit} and {shoes}.");
+                    }
+                }                         
+        }
+    }
+}
