1c67fbbd9ae06184f98138e6c7a295af5c85a259
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-16 01:25:52 +0300

    Add files via upload

---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 4/Lectures/Character Sequence/Character Sequence/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Character_Sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string  text = Console.ReadLine();
+
+            for(int i = 0; i < text.Length; i++)
+            {
+                Console.WriteLine(text[i]);
+            }
+        }
+    }
+}
---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 4/Lectures/Even Powers of 2/Even Powers of 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Numbers_1.N_with_Step_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int num = 1;
+
+            for(int counter = 0; counter <= number; counter +=2)
+            {
+                Console.WriteLine(num);
+                num *= 4;
+            }
+        }
+    }
+}
