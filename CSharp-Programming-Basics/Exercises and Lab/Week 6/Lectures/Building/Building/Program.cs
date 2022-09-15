e8ae75cae9101e31e8323e0f2a83155f02e882db
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-16 01:39:25 +0300

    Add files via upload

---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 6/Lectures/Building/Building/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Building
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            int floors = int.Parse(Console.ReadLine());
+            int rooms = int.Parse(Console.ReadLine());
+
+            for(int currFloor = floors; currFloor >= 1; currFloor--)
+            {
+                for(int currRoom = 0; currRoom < rooms; currRoom++)
+                {
+                    if (currFloor == floors)
+                    {
+                        Console.Write($"L{currFloor}{currRoom} ");
+                    }
+
+                    else if(currFloor % 2 == 0)
+                    {
+                        Console.Write($"O{currFloor}{currRoom} ");
+                    }
+
+                    else
+                    {
+                        Console.Write($"A{currFloor}{currRoom} ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ CSharp-Programming-Basics/Exercises and Lab/Week 6/Lectures/Combinations/Combinations/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Combinations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int magicNum = int.Parse(Console.ReadLine());
+            int combinations = 0;
+
+            for(int firstNum = 0; firstNum <= magicNum; firstNum++)
+            {
+                for(int secondNum = 0; secondNum <= magicNum; secondNum++)
+                {
+                    for(int thirdNum = 0; thirdNum <= magicNum; thirdNum++)
+                    {
+                        if(firstNum + secondNum + thirdNum == magicNum)
+                        {
+                            combinations++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(combinations);
+        }
+    }
+}
