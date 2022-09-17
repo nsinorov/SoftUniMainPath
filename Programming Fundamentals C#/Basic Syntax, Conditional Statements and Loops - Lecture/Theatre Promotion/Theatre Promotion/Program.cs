875897e20e4fa1e98090dc53a1b93ca3d8c46e06
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-17 16:53:02 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Basic Syntax, Conditional Statements and Loops - Lecture/Theatre Promotion/Theatre Promotion/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Theatre_Promotion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string dayOfWeek = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            int ticketPrice = 0;
+
+          
+
+            if (dayOfWeek == "Weekday")
+            {
+                if (age >= 0 && age <= 18)
+                {
+                    ticketPrice = 12;
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    ticketPrice = 18;
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    ticketPrice = 12;
+                }
+            }
+
+                else if (dayOfWeek == "Weekend")
+                {
+                    if (age >= 0 && age <= 18)
+                    {
+                        ticketPrice = 15;
+                    }
+                    else if (age > 18 && age <= 64)
+                    {
+                        ticketPrice = 20;
+                    }
+                    else if (age > 64 && age <= 122)
+                    {
+                        ticketPrice = 15;
+                    }
+
+                }
+
+                   else if (dayOfWeek == "Holiday")
+                   {
+                        if (age >= 0 && age <= 18)
+                        {
+                            ticketPrice = 5;
+                        }
+                        else if (age > 18 && age <= 64)
+                        {
+                            ticketPrice = 12;
+                        }
+                        else if (age > 64 && age <= 122)
+                        {
+                            ticketPrice = 10;
+                        }
+                   }
+            
+            if (age < 0 || age > 122)
+            {
+                Console.WriteLine("Error!");           
+            }
+            else
+            {
+                Console.WriteLine($"{ticketPrice}$");
+            }            
+        }
+    }
+}
