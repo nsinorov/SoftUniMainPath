856bf9f6ea9e4d471e643490a3d95591408d6d88
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-17 17:53:14 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Basic Syntax, Conditional Statements and Loops - Lecture/Sum of Odd Numbers/Sum of Odd Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Sum_of_Odd_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int startingNum = 1;
+
+            while(n > 0)
+            {
+                Console.WriteLine(startingNum);
+
+                sum += startingNum;
+
+                startingNum += 2;
+
+                n--;
+            }        
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
