13b76f7208db4d5f8128e38862eec06acd565232
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-17 19:26:13 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Basic Syntax, Conditional Statements and Loops - Lecture/Multiplication Table 2/Multiplication Table 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Multiplication_Table_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+
+            if(n2 <= 10)
+            {
+                for (int i = n2; i <= 10; i++)
+                {
+                    Console.WriteLine($"{n1} X {i} = {n1 * i}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{n1} X {n2} = {n1 * n2}");
+            }
+        }
+    }
+}
