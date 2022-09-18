60b9c4816e84fff5a04323392a10357a4f7aae49
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-18 19:38:18 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Basic Syntax, Conditional Statements and Loops - More Exercise/Engl Last Digit/Engl Last Digit/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Engl_Last_Digit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = int.Parse(Console.ReadLine());
+            int lastDigit = num % 10;
+
+            switch(num % 10)
+            {
+                case 0:
+                    Console.WriteLine("zero");
+                    break;
+                case 1:
+                    Console.WriteLine("one");
+                    break;
+                case 2:
+                    Console.WriteLine("two");
+                    break;
+                case 3:
+                    Console.WriteLine("three");
+                    break;
+                case 4:
+                    Console.WriteLine("four");
+                    break;
+                case 5:
+                    Console.WriteLine("five");
+                    break;
+                case 6:
+                    Console.WriteLine("six");
+                    break;
+                case 7:
+                    Console.WriteLine("seven");
+                    break;
+                case 8:
+                    Console.WriteLine("eight");
+                    break;
+                case 9:
+                    Console.WriteLine("nine");
+                    break;
+            }
+         
+        }
+    }
+}
