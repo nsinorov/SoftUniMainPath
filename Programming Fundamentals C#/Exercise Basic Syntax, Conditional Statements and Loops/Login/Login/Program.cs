4b0a2a35d8f9c12ef0d5c03672c405d0c3a9b147
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-18 06:00:54 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Exercise Basic Syntax, Conditional Statements and Loops/Login/Login/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string username = Console.ReadLine();
+            int usernameLenght = username.Length - 1;
+            string password = String.Empty;
+            int count = 0;
+
+            for (int i = usernameLenght; i >= 0; i--)
+            {
+                password += username[i];    
+            }
+            
+            string inputPass = Console.ReadLine();
+
+            while(inputPass != password)
+            {
+                count++;
+
+                if(count > 3)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    return;
+                }
+
+                Console.WriteLine("Incorrect password. Try again.");
+                inputPass = Console.ReadLine();
+            }
+
+            if(inputPass == password)
+            {
+                Console.WriteLine($"User {username} logged in.");
+            }      
+        }
+    }
+}
