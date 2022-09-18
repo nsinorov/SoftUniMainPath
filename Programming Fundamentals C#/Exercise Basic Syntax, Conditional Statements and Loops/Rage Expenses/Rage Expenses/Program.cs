b6e4389f544bc43e1bd00eb094995dc140a9361e
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-18 16:35:41 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Exercise Basic Syntax, Conditional Statements and Loops/Rage Expenses/Rage Expenses/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Rage_Expenses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int lostGames = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+            double headsetExpenses = lostGames / 2;
+            double mouseExpenses = lostGames / 3;
+            double keyboardExpenses = lostGames / 6;
+            double displayExpenses = lostGames / 12;
+
+            double expenses = (headsetExpenses * headsetPrice) + (mouseExpenses * mousePrice) + (keyboardExpenses * keyboardPrice) + (displayExpenses * displayPrice) ;
+
+            Console.WriteLine($"Rage expenses: {expenses:F2} lv.");
+
+        }
+    }
+}
