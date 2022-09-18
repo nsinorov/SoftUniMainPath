5e9c6e7c68412f38a1f75ff50900dae4bfa565d6
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-18 20:02:24 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Basic Syntax, Conditional Statements and Loops - More Exercise/Reverse String/Reverse String/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Reverse_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int inputLenght = input.Length - 1;
+            string inputReversed = "";
+
+            for (int i = inputLenght; i >= 0; i--)
+            {
+                inputReversed += input[i];
+            }
+            Console.WriteLine(inputReversed);
+
+        }
+    }
+}
