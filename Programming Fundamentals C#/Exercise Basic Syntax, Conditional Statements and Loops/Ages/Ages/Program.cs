15a1e1e7907a29b8e29f94d7119f875191641306
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-18 03:47:48 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Exercise Basic Syntax, Conditional Statements and Loops/Ages/Ages/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {         
+            int age = int.Parse(Console.ReadLine());
+
+            string result = age <= 2 ? "baby" : age > 2 && age <= 13 ? "child" : age > 13 && age <= 19 ? "teenager" : age > 19 && age <= 65 ? "adult" : "elder";
+
+            Console.WriteLine(result);
+        }
+    }
+}
