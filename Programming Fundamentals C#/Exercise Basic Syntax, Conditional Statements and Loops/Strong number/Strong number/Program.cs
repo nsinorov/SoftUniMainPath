d6d632dc85be6035814b47355e29b40ddcd199cb
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-18 15:33:00 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Exercise Basic Syntax, Conditional Statements and Loops/Strong number/Strong number/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Strong_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int strongNum = int.Parse(Console.ReadLine());
+
+            int strongNumCopy = strongNum;
+            int sum = 0;
+
+            while (strongNum > 0)
+            {
+                int factorialNum = 1;
+                int currNum = strongNum % 10;
+                strongNum /= 10;
+
+                for (int i = 2; i <= currNum; i++)
+                {
+                    factorialNum *= i;
+
+                }
+                sum += factorialNum;
+            }
+            Console.WriteLine(sum == strongNumCopy ? "yes" : "no");
+        }
+    }
+}
