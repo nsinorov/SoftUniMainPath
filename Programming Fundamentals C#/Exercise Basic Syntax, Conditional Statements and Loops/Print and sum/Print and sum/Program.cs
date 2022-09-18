7d2bc0174a4a9bd60cbc01033077685221e08770
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-18 04:37:20 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Exercise Basic Syntax, Conditional Statements and Loops/Print and sum/Print and sum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Print_and_sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int startingNum = int.Parse(Console.ReadLine());
+            int endNum = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = startingNum; i <= endNum; i++)
+            {
+                Console.Write($"{i} ");
+                sum += i;
+            }
+
+            Console.WriteLine();
+            Console.Write($"Sum: {sum}");
+        }
+    }
+}
