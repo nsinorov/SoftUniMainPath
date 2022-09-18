d7b05322727c7ef2249363fd7104139a2e203f95
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-18 06:15:16 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Exercise Basic Syntax, Conditional Statements and Loops/Triangle of Numbers/Triangle of Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Triangle_of_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int n = int.Parse(Console.ReadLine());
+
+            for (int row = 1; row <= n; row++)
+            {
+
+                for (int col = 1; col <= row; col++)
+                {
+                    Console.Write($"{row} ");
+                }
+
+                Console.WriteLine();
+              
+            }
+          
+        }
+    }
+}
