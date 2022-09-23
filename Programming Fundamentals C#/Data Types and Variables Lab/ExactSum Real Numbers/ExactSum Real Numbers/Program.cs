b4818ba92073b273d219daf292278bf7d0e23316
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-23 04:41:20 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Data Types and Variables Lab/ExactSum Real Numbers/ExactSum Real Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExactSum_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            decimal sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+
+              decimal currNum = decimal.Parse(Console.ReadLine());
+
+                sum += currNum;
+               
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
