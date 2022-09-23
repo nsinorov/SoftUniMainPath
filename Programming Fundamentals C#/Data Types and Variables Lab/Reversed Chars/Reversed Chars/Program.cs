34dc5a0c41d8d46ea0ea0d571c91226ec1a4edd8
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-23 05:19:55 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Data Types and Variables Lab/Reversed Chars/Reversed Chars/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Reversed_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {       
+            char firstInput = char.Parse(Console.ReadLine());
+            char secondInput = char.Parse(Console.ReadLine());
+            char thirdInput = char.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{thirdInput} {secondInput} {firstInput}");
+        }
+    }
+}
