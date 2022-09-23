542db442504b246b23fa18028903a85c3a18bbf4
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-23 05:23:46 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Data Types and Variables Lab/Concat Names/Concat Names/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Concat_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            string delimiter = Console.ReadLine();
+
+            Console.WriteLine($"{firstName}{delimiter}{lastName}");
+
+        }
+    }
+}
