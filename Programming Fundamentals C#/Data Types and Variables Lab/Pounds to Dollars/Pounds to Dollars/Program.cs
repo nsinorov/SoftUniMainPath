d29f67e47ae5a8c95c8499962f17729db172ce32
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-23 04:27:21 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Data Types and Variables Lab/Pounds to Dollars/Pounds to Dollars/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pounds_to_Dollars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal pounds = decimal.Parse(Console.ReadLine());
+            decimal dollars = pounds * 1.31M;
+
+            Console.WriteLine($"{dollars:f3}");      
+        }
+    }
+}
