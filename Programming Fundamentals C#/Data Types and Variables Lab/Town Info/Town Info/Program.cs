a64e9b395cbf95a910e888040b66f763808082c0
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-25 20:00:31 +0300

    Adding exercise Town Info

---
+++ Programming Fundamentals C#/Data Types and Variables Lab/Town Info/Town Info/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Town_Info
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {        
+            string nameOfTown = Console.ReadLine();
+            int population = int.Parse(Console.ReadLine());
+            int area = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Town {nameOfTown} has population of {population} and area {area} square km.");
+
+        }
+    }
+}
