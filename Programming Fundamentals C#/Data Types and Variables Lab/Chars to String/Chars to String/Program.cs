b94897c943446b8eb69cdc7203e5c5ef7be7102c
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-25 20:01:13 +0300

    Adding exercise Chars to String

---
+++ Programming Fundamentals C#/Data Types and Variables Lab/Chars to String/Chars to String/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Chars_to_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            char line1 = char.Parse(Console.ReadLine());
+            char line2 = char.Parse(Console.ReadLine());
+            char line3 = char.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{line1}{line2}{line3}");
+        }
+    }
+}
