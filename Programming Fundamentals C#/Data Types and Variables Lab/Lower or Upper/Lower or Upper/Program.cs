75d31c2ddff0445811061d75123831c8a75153cf
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-25 20:31:42 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Data Types and Variables Lab/Lower or Upper/Lower or Upper/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lower_or_Upper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            char input = char.Parse(Console.ReadLine());
+
+            if (Char.IsLower(input))
+            {
+                Console.WriteLine("lower-case");
+            }
+            else
+            {
+                Console.WriteLine("upper-case");
+            }
+        }
+    }
+}
