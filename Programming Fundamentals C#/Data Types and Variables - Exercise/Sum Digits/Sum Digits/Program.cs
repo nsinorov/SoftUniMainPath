b0eef74fbd89669167ae5203041ea14dd0241fad
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-26 18:53:17 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Data Types and Variables - Exercise/Sum Digits/Sum Digits/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Sum_Digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int num = int.Parse(Console.ReadLine());
+
+            int currNum = 0;
+            int sum = 0;
+
+            while (num > 0)
+            {
+                currNum = num % 10;
+                num /= 10;
+                sum += currNum;
+
+            }
+
+            Console.WriteLine(sum);         
+        }
+    }
+}
