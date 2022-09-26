9e23e157741c8e6bf713ec6f3590476adc5dd2df
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-26 23:34:33 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Data Types and Variables - Exercise/Print Part Of ASCII Table/Print Part Of ASCII Table/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Print_Part_Of_ASCII_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int startNum = int.Parse(Console.ReadLine());
+            int endNum = int.Parse(Console.ReadLine());
+
+            for (int i = startNum; i <= endNum; i++)
+            {
+                char character = (char)i;
+
+                Console.Write($"{character} ");
+            }     
+        }
+    }
+}
