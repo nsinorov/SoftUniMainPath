c43594bd5eb032b99c685df180b1c46e8eaf673d
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-26 23:22:09 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Data Types and Variables - Exercise/Elevator/Elevator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            int numOfPeople = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            double courses = (double)numOfPeople / capacity;
+
+            Console.WriteLine(Math.Ceiling(courses));
+        }
+    }
+}
