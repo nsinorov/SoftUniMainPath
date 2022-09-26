df553261067c0601ce246c05fced6f44badbc35a
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-26 23:29:17 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Data Types and Variables - Exercise/Sum of Chars/Sum of Chars/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Sum_of_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                char currLetter = char.Parse(Console.ReadLine());
+                sum += (int) currLetter;
+
+
+            }
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
