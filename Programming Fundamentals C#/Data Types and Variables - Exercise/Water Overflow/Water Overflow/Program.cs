12ef753264e4b9f41875a4e65ba6eb2abb53ced7
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-26 23:51:51 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Data Types and Variables - Exercise/Water Overflow/Water Overflow/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Water_Overflow
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            const int CAPACITY = 255;
+            int totalCapacity = CAPACITY;
+        
+            int nLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < nLines; i++)
+            {
+                int quantityOfWater = int.Parse((Console.ReadLine()));
+
+                if(totalCapacity - quantityOfWater >= 0)
+                {
+                    totalCapacity -= quantityOfWater;
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+
+            }
+
+            int totalFilledQuantity = CAPACITY - totalCapacity;
+            Console.WriteLine(totalFilledQuantity);
+        }
+    }
+}
