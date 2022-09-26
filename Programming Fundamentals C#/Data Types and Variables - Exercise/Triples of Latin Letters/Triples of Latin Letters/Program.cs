8b139cbe4335c2425447c6dfd05389579de04343
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-26 23:41:36 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Data Types and Variables - Exercise/Triples of Latin Letters/Triples of Latin Letters/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Triples_of_Latin_Letters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <n ; i++)
+            {
+                char firstChar = (char) ('a' + i);
+                for (int j = 0; j <n ; j++)
+                {
+                    char secondChar = (char)('a' + j);
+                    for (int k = 0; k <n ; k++)
+                    {
+                        char thirdChar = (char)('a' + k);
+
+                        Console.WriteLine($"{firstChar}{secondChar}{thirdChar}");
+                    }
+                }
+            }
+           
+        }
+    }
+}
