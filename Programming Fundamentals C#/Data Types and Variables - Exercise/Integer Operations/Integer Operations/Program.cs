9866c9ff5b78129ca678a315d51477bec08329fe
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-26 18:37:04 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Data Types and Variables - Exercise/Integer Operations/Integer Operations/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Integer_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int num4 = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            sum = num1 + num2;
+
+            int divide = 0;
+            divide = sum / num3;
+
+            int multiply = 0;
+            multiply = divide * num4;
+
+            Console.WriteLine(multiply);
+        }
+    }
+}
