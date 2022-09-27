39ee7323089afaa7e5b69cf7ad8993bf1f2b621d
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-27 23:15:15 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Data Types and Variables - Exercise/Snowballs/Snowballs/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+
+namespace Snowballs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {          
+            int snowBalls = int.Parse(Console.ReadLine());
+
+            BigInteger snowBallValue = 0;
+            BigInteger snowBallSnow = 0;
+            BigInteger snowBalTime = 0;
+            int snowBallQuality = 0;
+            BigInteger bestSnowBall = int.MinValue;
+            string bestSnowBallFormula = "";
+
+            for (int i = 0; i < snowBalls; i++)
+            {
+                snowBallSnow = BigInteger.Parse(Console.ReadLine());
+                snowBalTime = BigInteger.Parse(Console.ReadLine());
+                snowBallQuality = int.Parse(Console.ReadLine());
+
+                BigInteger currentSnowValue = snowBallSnow / snowBalTime;
+                snowBallValue = BigInteger.Pow(currentSnowValue, snowBallQuality);
+
+                if(snowBallValue > bestSnowBall)
+                {
+                    bestSnowBall = snowBallValue;
+                    bestSnowBallFormula = $"{snowBallSnow} : {snowBalTime} = {snowBallValue} ({snowBallQuality})";
+                }
+
+            }
+            Console.WriteLine(bestSnowBallFormula);
+        }
+    }
+}
