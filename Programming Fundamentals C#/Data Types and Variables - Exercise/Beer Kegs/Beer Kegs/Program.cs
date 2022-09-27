7e2eebeb41d903d581b7cdae9304e5c8bb094513
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-27 16:59:31 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Data Types and Variables - Exercise/Beer Kegs/Beer Kegs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Beer_Kegs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            int nLines = int.Parse(Console.ReadLine());
+
+            double biggestKeg = double.MinValue;
+            string biggestKegName = "";
+
+            for (int i = 0; i < nLines; i++)
+            {
+
+                string model = Console.ReadLine();
+                float radius = float.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                double volumeOfTheKeg = Math.PI * Math.Pow(radius, 2) * height;
+
+                if(volumeOfTheKeg > biggestKeg)
+                {
+                    biggestKeg = volumeOfTheKeg;
+                    biggestKegName = model;
+                }
+            }
+            Console.WriteLine(biggestKegName);
+        }
+    }
+}
