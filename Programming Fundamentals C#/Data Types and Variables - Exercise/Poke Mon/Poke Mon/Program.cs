b14f851915b72af4a022c6f3766a2cf6633d9c2f
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-27 18:08:43 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Data Types and Variables - Exercise/Poke Mon/Poke Mon/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Poke_Mon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int power = int.Parse(Console.ReadLine());
+            int distance = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+
+            int startingPower = power;
+            int countOfPokedTargets = 0;
+
+            while(power >= distance)
+            {
+                power -= distance;
+                countOfPokedTargets++;
+
+                if(startingPower * 0.5 == power && exhaustionFactor > 0)
+                {
+                    power /= exhaustionFactor;
+                }
+
+            }
+            Console.WriteLine(power);
+            Console.WriteLine(countOfPokedTargets);
+        }
+    }
+}
