b14803f7ea05a93d0297d3e641135ca0ca0b09dc
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-29 02:57:51 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Data Types and Variables - More Exercise/Decrypting Messages/Decrypting Messages/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Decrypting_Messages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {         
+            int key = int.Parse(Console.ReadLine());
+            int nLines = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            string result = "";
+
+            for (int i = 0; i < nLines; i++)
+            {
+                char characters = char.Parse(Console.ReadLine());
+
+                sum = (int)characters + key;
+                result += (char)sum;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
