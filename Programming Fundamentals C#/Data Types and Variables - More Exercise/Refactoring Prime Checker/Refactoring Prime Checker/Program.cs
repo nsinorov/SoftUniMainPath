04547678d664500ef55bd25c3dcb4ab7e5601539
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-29 02:33:47 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Data Types and Variables - More Exercise/Refactoring Prime Checker/Refactoring Prime Checker/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Refactoring_Prime_Checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 2; i <= num; i++)
+            {
+                bool isPrime = true;
+
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                Console.WriteLine($"{i} -> {isPrime.ToString().ToLowerInvariant()}");
+            }
+        }     
+    }
+}
