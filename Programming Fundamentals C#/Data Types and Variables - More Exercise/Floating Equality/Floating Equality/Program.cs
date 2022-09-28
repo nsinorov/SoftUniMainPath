bb930aa26e708bd05cd06fe90d14f574a54e84e7
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-29 02:33:18 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Data Types and Variables - More Exercise/Floating Equality/Floating Equality/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Floating_Equality
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double EPS = 0.000001;
+
+            double number1 = double.Parse(Console.ReadLine());
+            double number2 = double.Parse(Console.ReadLine());
+
+            double difference = Math.Abs(number1 - number2);
+
+            if (difference < EPS)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
