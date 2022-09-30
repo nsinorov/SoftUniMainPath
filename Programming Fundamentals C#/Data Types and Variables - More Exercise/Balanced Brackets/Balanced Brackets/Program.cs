8ec16dc1ae76d449a9ffc0787071f395fd29ec5f
Nikolay Sinorov <nsinorov123@gmail.com>
2022-09-30 03:11:46 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Data Types and Variables - More Exercise/Balanced Brackets/Balanced Brackets/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Balanced_Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            int openCount = 0;
+            int closeCount = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "(")
+                {
+                    openCount++;
+                }
+
+                if (input == ")")
+                {
+                    closeCount++;
+
+                    if (openCount - closeCount != 0)
+                    {
+                        break;
+                    }
+                }
+            }
+           string result = openCount == closeCount ? "BALANCED" : "UNBALANCED";
+           Console.WriteLine(result);
+        }
+    }
+}
