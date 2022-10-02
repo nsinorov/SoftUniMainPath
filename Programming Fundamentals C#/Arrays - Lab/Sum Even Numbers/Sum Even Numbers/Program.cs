be5e64d6436f2c654cd6444ddc63e36cf42706cb
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-02 08:14:39 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Arrays - Lab/Sum Even Numbers/Sum Even Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Sum_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int sum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int currNum = nums[i];
+                if(currNum % 2 == 0)
+                {
+                    sum += currNum;
+                }             
+            }
+            Console.Write(sum);
+        }
+    }
+}
