990fc17f76fecc51f8cd9f46c3a0628e6fb93ea8
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-02 07:46:37 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Arrays - Lab/Reverse Array of Strings/Reverse Array of Strings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Reverse_Array_of_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ').ToArray();
+
+            for (int i = 0; i < input.Length / 2; i++)
+            {
+                var oldElement = input[i];
+                input[i] = input[input.Length - 1 - i];
+                input[input.Length - 1 - i] = oldElement;
+
+            }
+            Console.Write(string.Join(" ", input));
+        }
+    }
+}
