1c5cbc8cbf6a4e3f6068190e0c6b65f8f0d78849
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-02 07:31:03 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Arrays - Lab/Rounding Numbers/Rounding Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Rounding_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] nums = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine($"{nums[i]} => { (int)Math.Round(nums[i], MidpointRounding.AwayFromZero) }");
+            }
+        }
+    }
+}
