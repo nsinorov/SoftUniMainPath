fa52c476c166961e9b3d960f695578ee4c93677b
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-02 17:30:27 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Arrays - Lab/Condense Array to Number/Condense Array to Number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Condense_Array_to_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {         
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int count = nums.Length;
+             
+            while(count > 1)
+            {
+                for (int i = 0; i < count - 1; i++)
+                {
+                    nums[i] = nums[i] + nums[i + 1];
+                }
+                count--;                     
+            }
+            Console.WriteLine(nums[0]);
+        }
+    }
+}
