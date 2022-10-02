abca25152db02e25b22d15878198b8713d4256e5
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-02 09:03:10 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Arrays - Lab/Equal Arrays/Equal Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Equal_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] arr2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+
+                if (arr1[i] != arr2[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    return;
+                }                          
+            }
+            int sum = arr1.Sum();
+            Console.WriteLine($"Arrays are identical. Sum: {sum}");          
+        }
+    }
+}
