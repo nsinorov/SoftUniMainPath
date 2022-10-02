7a9002454783a59bb6f8e39ae037cd94839b8301
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-02 03:57:00 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Arrays - Lab/Print Numbers in Reverse Order/Print Numbers in Reverse Order/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Print_Numbers_in_Reverse_Order
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+
+                arr[i] = int.Parse(Console.ReadLine());
+
+            }
+            for (int i = n - 1; i >= 0; i--)
+            {
+                Console.Write($"{arr[i]} ");
+
+            }
+        }
+    }
+}
