d0bc3b908e37ced2960e9ad4363a8fd52f02f3be
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-02 08:29:41 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Arrays - Lab/Even and Odd Subtraction/Even and Odd Subtraction/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Even_and_Odd_Subtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int sumForEven = 0;
+            int sumForOdd = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int currNum = nums[i];
+
+                if (currNum % 2 == 0)
+                {
+                    sumForEven += currNum;
+                }
+                else
+                {
+                    sumForOdd += currNum;
+                }
+            }
+            Console.Write($"{sumForEven - sumForOdd}");
+        }
+    }
+}
