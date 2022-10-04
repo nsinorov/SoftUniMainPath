90333d22ec3d25146ed4ef6aedaa430325e3b658
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-05 01:09:27 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Arrays - Exercise/Top Integers/Top Integers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Top_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                bool currNumIsBigger = true;
+
+                for (int j = i + 1; j < input.Length; j++)
+                {
+                    if(input[i] <= input[j])
+                    {
+                        currNumIsBigger = false;
+                        break;
+                    }
+                }
+                if (currNumIsBigger)
+                {
+                    Console.Write($"{input[i]} ");
+                }
+            }
+        }
+    }
+}
