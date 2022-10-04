8318fc051715da850e6291b6b60c799918dab453
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-04 22:25:33 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Arrays - Exercise/Train/Train/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Train
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int numOfWagons = int.Parse(Console.ReadLine());
+
+            int[] countOfPassengers = new int[numOfWagons];
+            int sum = 0;
+
+            for (int i = 0; i < numOfWagons; i++)
+            {
+                int currPassengers = int.Parse(Console.ReadLine());
+                countOfPassengers[i] += currPassengers;
+                sum += currPassengers;
+                
+            }
+            Console.WriteLine(String.Join(" ", countOfPassengers));
+            Console.WriteLine(sum);
+        }
+    }
+}
