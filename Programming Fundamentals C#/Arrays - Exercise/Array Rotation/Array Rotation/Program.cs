0277510e84d7447051b16f0dac84760cfe7c07a9
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-05 00:47:20 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Arrays - Exercise/Array Rotation/Array Rotation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Array_Rotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int rotations = int.Parse(Console.ReadLine());
+
+            for (int rotarion = 0; rotarion < rotations; rotarion++)
+            {
+                int tempElement = input[0];
+
+                for (int i = 0; i < input.Length - 1; i++)
+                {
+                    input[i] = input[i + 1];
+                }
+
+                input[input.Length - 1] = tempElement;
+            }
+            Console.WriteLine(String.Join(" ", input));
+        }
+    }
+}
