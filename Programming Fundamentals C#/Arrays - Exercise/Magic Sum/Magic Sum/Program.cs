358c8b5f7eb99bbc97eda274d82bd08f9020799b
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-05 02:02:13 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Arrays - Exercise/Magic Sum/Magic Sum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Magic_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int sumNum = int.Parse(Console.ReadLine());
+        
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                for (int j = 1 + i; j < input.Length; j++)
+                {
+                    if (input[i] + input[j] == sumNum)
+                    {
+                        Console.WriteLine($"{input[i]} {input[j]}");
+                    }
+                }      
+            }
+        }
+    }
+}
