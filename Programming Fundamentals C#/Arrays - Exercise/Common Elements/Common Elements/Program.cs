10e3e1827ab0a72d85371c327057733067752305
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-04 22:44:01 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Arrays - Exercise/Common Elements/Common Elements/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Common_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr1 = Console.ReadLine().Split();
+            string[] arr2 = Console.ReadLine().Split();
+
+             foreach (string currElement in arr1)
+             {
+                for (int i = 0; i < arr2.Length; i++)
+                {
+                    string secondCurrElement = arr2[i];
+
+                    if(currElement == secondCurrElement)
+                    {
+                        Console.Write($"{currElement} ");
+                       break;
+                    }
+                }
+             }
+        }
+    }
+}
