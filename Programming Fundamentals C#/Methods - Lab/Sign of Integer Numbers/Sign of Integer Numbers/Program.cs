3c1f5a1000f54dd89e2ca7f357b4666da7f4d698
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-08 14:44:09 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Methods - Lab/Sign of Integer Numbers/Sign of Integer Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Sign_of_Integer_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            checkNum(num);
+        }
+        static void checkNum(int num)
+        {
+            if (num == 0)
+            {
+                Console.WriteLine($"The number {num} is zero.");
+            }
+            else if (num > 0)
+            {
+                Console.WriteLine($"The number {num} is positive.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {num} is negative.");
+            }
+        }
+    }
+}
