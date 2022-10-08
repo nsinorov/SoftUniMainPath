f7b8fc0343037e8c9afd957da38c8ff0d70411ce
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-09 01:07:07 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Methods - Lab/Repeat String/Repeat String/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Repeat_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
+
+            Output(input, count);
+
+        }
+         static void Output(string input, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(input);
+            }
+        }
+    }
+}
