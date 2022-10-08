74ce90ad4fada1e78f1050881eb01bb90cce5ff8
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-09 01:02:39 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Methods - Lab/Calculate Rectangle Area/Calculate Rectangle Area/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Calculate_Rectangle_Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int result = RectangleArea(a, b);
+
+            Console.WriteLine(result);
+        }
+
+         static int RectangleArea(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
