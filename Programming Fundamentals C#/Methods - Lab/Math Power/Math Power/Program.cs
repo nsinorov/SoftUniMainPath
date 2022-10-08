f444718d3aaaff27a643f820352eea84fd9b9de3
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-09 01:39:38 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Methods - Lab/Math Power/Math Power/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Math_Power
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numBase = double.Parse(Console.ReadLine());
+            int numPower = int.Parse(Console.ReadLine());
+        
+            double result = PowerUp(numBase, numPower);
+            Console.WriteLine(result);
+        }
+         static double PowerUp(double numBase, int numPower)
+        {
+            return Math.Pow(numBase, numPower);
+        }
+    }
+}
