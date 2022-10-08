fd15fa756d81b1702bcb2a3944491b58112a8b0f
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-08 14:55:39 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Methods - Lab/Grades/Grades/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+
+            checkGrade(grade);
+        }
+
+        private static void checkGrade(double grade)
+        {
+            string result = grade >= 2.00 && grade <= 2.99 ? "Fail" :
+                            grade >= 3.00 && grade <= 3.49 ? "Poor" :
+                            grade >= 3.50 && grade <= 4.49 ? "Good" :
+                            grade >= 4.50 && grade <= 5.49 ? "Very good" :
+                            grade >= 5.50 && grade <= 6.00 ? "Excellent" : "Invalid grade";
+
+            Console.WriteLine(result);
+        }
+    }
+}
