95405c979e59f5ec307deb33a5365aad6b3d29c0
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-08 15:21:59 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Methods - Lab/Printing Triangle/Printing Triangle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Printing_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {       
+            int num = int.Parse(Console.ReadLine());
+
+            for (int rows = 1; rows <= num; rows++)
+            {
+                PrintColumns(rows);
+            }
+
+            for (int rows = num - 1; rows >= 1; rows--)
+            {
+                PrintColumns(rows);
+            }
+        }
+        private static void PrintColumns(int rows)
+        {
+            for (int cols = 1; cols <= rows; cols++)
+            {
+                Console.Write($"{cols} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
