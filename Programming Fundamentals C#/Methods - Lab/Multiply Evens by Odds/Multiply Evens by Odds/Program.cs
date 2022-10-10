adb858388f159a9d131e59ada79386b9b6a57f38
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-10 13:41:31 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Methods - Lab/Multiply Evens by Odds/Multiply Evens by Odds/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Multiply_Evens_by_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int absValue = Math.Abs(int.Parse(input));
+
+            int sumOfEvenNums = GetSumOfEvenNums(absValue.ToString());
+            int sumOfOddNums = GetSumOfOddDigits(absValue.ToString());
+
+            int result = sumOfEvenNums * sumOfOddNums;
+            Console.WriteLine(result);
+
+        }
+
+         static int GetSumOfEvenNums(string input)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int currNum = int.Parse(input[i].ToString());
+                if(currNum % 2 == 0)
+                {
+                    sum += currNum;
+                }           
+            }
+            return sum;
+        }
+         static int GetSumOfOddDigits(string input)
+        {
+
+            int sum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int currNum = Math.Abs(int.Parse(input[i].ToString()));
+                if (currNum % 2 != 0)
+                {
+                    sum += currNum;
+                }
+            }
+            return sum;
+         }
+    }
+}
