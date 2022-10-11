3456963b4019090724f06427bf874c688ed65b4b
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-11 19:59:37 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Methods - Exercise/Smallest of Three Numbers/Smallest of Three Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Smallest_of_Three_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {     
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(SmallestNum(num1, num2, num3));       
+        }
+        static int SmallestNum(int num1, int num2, int num3) => Math.Min(num1, Math.Min(num2, num3));                   
+    }
+}
