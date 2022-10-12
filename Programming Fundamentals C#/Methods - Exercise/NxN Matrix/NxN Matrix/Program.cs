28916862c74a79799c80d1ef60b894d95db5137e
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-13 01:30:33 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Methods - Exercise/NxN Matrix/NxN Matrix/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NxN_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            MatrixOfNums(n);
+
+
+
+        }
+
+         static void MatrixOfNums(int n)
+        {
+
+            for (int rows = 0; rows < n ; rows++)
+            {
+                for (int cols = 0; cols < n; cols++)
+                {
+                    Console.Write(n + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
