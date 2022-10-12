c7fdb59649723d25bc8d513aa05188649af8b238
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-13 01:51:22 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Methods - Exercise/Palindrome Integers/Palindrome Integers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Palindrome_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string input = Console.ReadLine();
+
+            while(input != "END")
+            {
+                bool isIntPalindrome = IsNumberPalindrome(input);
+                Console.WriteLine(isIntPalindrome.ToString().ToLower());
+                input = Console.ReadLine();
+            }
+
+
+        }
+        private static bool IsNumberPalindrome(string input)
+        {
+          int num = int.Parse(input);
+
+            if(num >= 0 && num <= 9)
+            {
+                return true;
+            }
+
+            for (int i = 0; i < input.Length / 2; i++)
+            {
+                if (input[i] == input[input.Length - 1])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
