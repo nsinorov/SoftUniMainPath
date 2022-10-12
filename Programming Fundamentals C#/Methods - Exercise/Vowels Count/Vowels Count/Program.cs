9b39f5e4582273bd49eb2fb828c8492785e9997c
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-13 00:40:50 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Methods - Exercise/Vowels Count/Vowels Count/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Vowels_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {        
+            string input = Console.ReadLine().ToLower();
+            SearchForVowels(input);
+        }
+
+         static void SearchForVowels(string input)
+        {
+            Console.WriteLine(input.Count(vowels => "aouei".Contains(vowels)));
+        }
+    }
+}
