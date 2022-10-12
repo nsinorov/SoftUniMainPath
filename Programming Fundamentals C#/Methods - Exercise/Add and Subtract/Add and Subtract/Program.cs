25439a21ccd923fb7620925c5b26833a16f42cc1
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-13 01:17:21 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Methods - Exercise/Add and Subtract/Add and Subtract/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Add_and_Subtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {       
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+           int result = Add(num1, num2);
+            Substract(result, num3);
+        }
+         static void Substract(int result, int num3) => Console.WriteLine(result - num3);
+        static int Add(int num1, int num2) => num1 + num2;    
+    }
+}
