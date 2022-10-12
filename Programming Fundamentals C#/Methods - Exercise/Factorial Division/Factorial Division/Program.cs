8981c7527b9b0e9b1e517582999fd748009b0aa4
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-13 01:35:24 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Methods - Exercise/Factorial Division/Factorial Division/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Factorial_Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            double result1 = Factorial(num1);
+            double resilt2 = Factorial(num2);
+
+            Console.WriteLine($"{result1 / resilt2:F2}");
+
+        }
+        private static double Factorial(int num1)
+        {
+           double result = 1;
+
+            while(num1 != 1)
+            {
+                result *= num1;
+                num1--;
+            }
+            return result;
+        }
+    }
+}
