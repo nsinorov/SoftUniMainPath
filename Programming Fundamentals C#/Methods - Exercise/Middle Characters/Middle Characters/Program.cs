e1ccf5cbd563d9b2bfe74cd441f775df7581cbae
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-13 01:22:54 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Methods - Exercise/Middle Characters/Middle Characters/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Middle_Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+            PrintMiddleChar(input);
+
+        }
+        private static void PrintMiddleChar(string input)
+        {
+            if(input.Length % 2 == 0)
+            {
+                Console.Write(input[input.Length / 2 - 1]);
+                Console.WriteLine(input[input.Length / 2]);
+            }
+            else
+            {
+                Console.WriteLine(input[input.Length / 2]);
+            }
+        }
+    }
+}
