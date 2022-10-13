2aaf5969d9ea220fef8d2545f59ec09a9076c7b4
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-13 04:50:25 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Methods - More Exercise/Data Types/Data Types/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            string typeOfData = Console.ReadLine();
+
+            if(typeOfData == "int")
+            {
+                int num = int.Parse(Console.ReadLine());
+                Console.WriteLine(num * 2);
+            }
+
+            if(typeOfData == "real")
+            {
+                double num = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{num * 1.5:F2}");
+            }
+
+            if(typeOfData == "string")
+            {
+                string input = Console.ReadLine();
+                Console.WriteLine($"${input}$");
+            }
+        }
+    }
+}
