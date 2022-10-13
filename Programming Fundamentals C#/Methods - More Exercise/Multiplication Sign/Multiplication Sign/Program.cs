8dfe9a31b621e3bca55a54df845c7be9c107df4a
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-13 05:12:59 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Methods - More Exercise/Multiplication Sign/Multiplication Sign/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Multiplication_Sign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {      
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            ResultSign(num1, num2, num3);
+        }
+        public static void ResultSign(double n1, double n2, double n3)
+        {
+            if (n1 == 0 || n2 == 0 || n3 == 0)
+            {
+                Console.WriteLine("zero");
+            }
+            else if ((n1 > 0 && n2 > 0 && n3 > 0) || (n1 < 0 && n2 < 0 && n3 > 0) || (n1 < 0 && n2 > 0 && n3 < 0) || (n1 > 0 && n2 < 0 && n3 < 0))
+            {
+                Console.WriteLine("positive");
+            }
+            else
+            {
+                Console.WriteLine("negative");
+            }
+        }
+    }
+}
