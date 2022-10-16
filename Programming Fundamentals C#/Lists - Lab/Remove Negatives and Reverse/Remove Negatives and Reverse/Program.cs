49e36813ade1114c9dcc6cdcf7161ffc74a57566
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-16 17:04:51 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Lists - Lab/Remove Negatives and Reverse/Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Remove_Negatives_and_Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            numbers.RemoveAll(x => x < 0);
+            numbers.Reverse();
+
+            Console.WriteLine(string.Join(" ", numbers));
+
+            if (numbers.Count == 0 || numbers.Count < 0)
+            {
+                Console.Write("empty");
+            }
+           
+        }
+    }
+}
