55dfc5378fc8fda3cf6e0a12781228def595cf59
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-16 16:34:07 +0300

    Add files via upload

---
+++ Programming Fundamentals C#/Lists - Lab/Gauss' Trick/Gauss' Trick/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Gauss__Trick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < numbers.Count / 2; i++)
+            {
+
+                int currSum = numbers[i] + numbers[numbers.Count - 1 - i];
+
+                result.Add(currSum);
+            }
+
+            if(numbers.Count % 2 != 0)
+            {
+                result.Add(numbers[numbers.Count / 2]);
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
