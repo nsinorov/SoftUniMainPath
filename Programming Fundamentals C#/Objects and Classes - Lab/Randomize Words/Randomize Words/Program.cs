7eea3640ef1a83a078a491e0cc9a1d564de4ed2d
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-30 23:35:13 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Objects and Classes - Lab/Randomize Words/Randomize Words/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Randomize_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+       
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                Random random = new Random();
+
+                int randomIndex = random.Next(0, input.Length);
+
+                string currentWord = input[i];
+                string nextWord = input[randomIndex];
+
+                input[i] = nextWord;
+                input[randomIndex] = currentWord;
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, input));
+        }
+    }
+}
