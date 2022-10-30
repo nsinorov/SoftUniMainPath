473bc98cac5c0e372fd93c46a9ab98041e825941
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-30 23:46:27 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Objects and Classes - Lab/Big Factorial/Big Factorial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+namespace Big_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = int.Parse(Console.ReadLine());
+
+            BigInteger factorial = 1;
+
+            for (int i = 1; i <= num ; i++)
+            {
+                factorial *= i;
+            }
+            Console.WriteLine(factorial);
+        }
+    }
+}
