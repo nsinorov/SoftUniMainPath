1d11b1ca244eca6ce4a1019ae427bc482d4e0ee8
Nikolay Sinorov <nsinorov123@gmail.com>
2022-11-12 13:52:44 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Objects and Classes - Lab/Songs/Songs/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Songs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {         
+            int numOfSongs = int.Parse(Console.ReadLine());
+
+            List<Song> songs = new List<Song>();
+
+            for (int i = 0; i < numOfSongs; i++)
+            {
+                string[] input = Console.ReadLine().Split("_");
+                string typeList = input[0];
+                string name = input[1];
+                string time = input[2];
+
+                Song song = new Song(typeList, name, time);
+                songs.Add(song);
+
+            }
+
+            string list = Console.ReadLine();
+
+            for (int i = 0; i < songs.Count; i++)
+            {
+                Song currentSong = songs[i];
+
+                if (list == "all")
+                {
+                    Console.WriteLine(currentSong.Name);
+                }
+                else if (list == currentSong.TypeList)
+                {
+                    Console.WriteLine(currentSong.Name);
+                }                    
+            }
+        }
+    }
+
+    public class Song
+    {
+        public Song(string typeList, string name, string time)
+        {
+            TypeList = typeList;
+            Name = name;
+            Time = time;
+
+        }
+
+        public string TypeList { get; set; }
+
+        public string Name { get; set; }
+
+        public string Time { get; set; }
+
+    }
+}
