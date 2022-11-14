6e36a92e403c5f9c0fa7f8919455ea2b2140facf
Nikolay Sinorov <nsinorov123@gmail.com>
2022-11-14 16:24:33 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Associative Arrays - Lab/Odd Occurrences/Odd Occurrences/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Odd_Occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var counts = new Dictionary<string, int>();
+
+            string[] words = Console.ReadLine().Split();
+
+            foreach (var word in words)
+            {
+                string wordInLowerCase = word.ToLower();
+
+                if (counts.ContainsKey(wordInLowerCase))
+                {
+                    counts[wordInLowerCase]++;
+                }
+                else
+                {
+                    counts.Add(wordInLowerCase, 1);
+                }
+            }
+
+            foreach (var count in counts)
+            {
+                if(count.Value % 2 != 0)
+                {
+                    Console.Write(count.Key + " ");
+                }
+            }
+        }
+    }
+}
