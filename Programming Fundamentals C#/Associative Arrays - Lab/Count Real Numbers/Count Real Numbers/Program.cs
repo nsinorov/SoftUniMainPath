4980c75de0d0f558bcad9b758b64ef6077d60f2d
Nikolay Sinorov <nsinorov123@gmail.com>
2022-11-14 16:23:05 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Associative Arrays - Lab/Count Real Numbers/Count Real Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Count_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {        
+            var numOfOcc = new SortedDictionary<double, int>();
+
+            double[] nums = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            foreach (var num in nums)
+            {          
+                if (!numOfOcc.ContainsKey(num))
+                {
+                     numOfOcc.Add(num, 0);   
+                }
+                numOfOcc[num] += 1;
+            }
+   
+            foreach (var item in numOfOcc)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
