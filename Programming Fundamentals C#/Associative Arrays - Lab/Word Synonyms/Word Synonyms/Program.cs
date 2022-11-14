dbbb5bb3d5fdc72e3154124eebe5e977e8f7cf3a
Nikolay Sinorov <nsinorov123@gmail.com>
2022-11-14 16:25:13 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Associative Arrays - Lab/Word Synonyms/Word Synonyms/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Word_Synonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {          
+            var synonims = new Dictionary<string, List<string>>();
+
+            int countOfWords = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < countOfWords; i++)
+            {
+                string word = Console.ReadLine();
+                string synonim = Console.ReadLine();
+
+                if (!synonims.ContainsKey(word))
+                {
+                    synonims.Add(word, new List<string>());
+                }
+                synonims[word].Add(synonim);
+            }
+
+            foreach (var item in synonims)
+            {
+                Console.WriteLine($"{item.Key} - {string.Join(", ", item.Value)}");
+            }
+        }
+    }
+}
