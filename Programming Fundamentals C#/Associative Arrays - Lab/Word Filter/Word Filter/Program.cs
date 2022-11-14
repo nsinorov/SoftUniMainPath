485a0876151cd2e487061cd8909c576a26eda6ec
Nikolay Sinorov <nsinorov123@gmail.com>
2022-11-14 16:25:52 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Associative Arrays - Lab/Word Filter/Word Filter/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Word_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine().Split().Where(x => x.Length % 2 == 0).ToList().ForEach(Console.WriteLine);       
+        }
+    }
+}
