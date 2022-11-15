fbe9fa758136f5eefa318ea78d80402702e64280
Nikolay Sinorov <nsinorov123@gmail.com>
2022-11-15 02:27:40 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Associative Arrays - Exercise/Company Users/Company Users/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Company_Users
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var companies = new Dictionary<string, List<string>>();
+
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                string[] tokens = command.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                string companyName = tokens[0];
+                var employeeID = tokens[1];
+
+                if (!companies.ContainsKey(companyName))
+                {
+                    companies.Add(companyName, new List<string>());
+
+                }
+
+                if (companies[companyName].Contains(employeeID))
+                {
+                    command = Console.ReadLine();
+                    continue;
+                }
+                   
+                companies[companyName].Add(employeeID);
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var company in companies)
+            {
+                Console.WriteLine($"{company.Key}");
+
+                foreach (var employeeID in company.Value)
+                {
+                    Console.WriteLine($"-- {employeeID}");
+                }
+            }
+        }
+    }
+}
