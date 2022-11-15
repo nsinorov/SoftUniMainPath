f761b82059ef1b1c2ee667272652a5c44a08908d
Nikolay Sinorov <nsinorov123@gmail.com>
2022-11-15 02:26:46 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Associative Arrays - Exercise/A Miner Task/A Miner Task/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace A_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            var resource = new Dictionary<string, int>();
+
+            while(input != "stop")
+            {
+                int currValue = int.Parse(Console.ReadLine());
+
+                if (!resource.ContainsKey(input))
+                {
+                    resource.Add(input, 0);
+                }
+                resource[input] += currValue;
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var item in resource)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
