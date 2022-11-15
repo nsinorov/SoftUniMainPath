cc0d9a345b7bcff8254d64ff1e52e27688001c2a
Nikolay Sinorov <nsinorov123@gmail.com>
2022-11-15 02:29:31 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Associative Arrays - Exercise/Student Academy/Student Academy/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Student_Academy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {      
+            var students = new Dictionary<string, List<double>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string studentName = Console.ReadLine();
+                double studentGrade = double.Parse(Console.ReadLine());
+
+                if (!students.ContainsKey(studentName))
+                {
+                    students[studentName] = new List<double>();               
+                }
+                students[studentName].Add(studentGrade);
+            }
+
+            foreach (var student in students)
+            {
+                string studentName = student.Key;
+                double studentGrade = student.Value.Average();
+
+                if(studentGrade >= 4.5)
+                {
+                    Console.WriteLine($"{studentName} -> {studentGrade:F2}");
+                }
+            }
+        }
+    }
+}
