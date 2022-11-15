bb914a10f96574a286d67617d655053feaea40ba
Nikolay Sinorov <nsinorov123@gmail.com>
2022-11-15 02:27:15 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Associative Arrays - Exercise/Count Chars in a String/Count Chars in a String/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Count_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            var final = new Dictionary<char, int>();
+
+            foreach (var ch in text)
+            {
+                if (!final.ContainsKey(ch))
+                {
+                    final.Add(ch, 1);
+                }
+                else if (final.ContainsKey(ch))
+                {
+                    final[ch]++;
+                }
+            }
+
+            foreach (KeyValuePair<char, int> ch in final.Where(a => a.Key != ' '))
+            {
+                Console.WriteLine($"{ch.Key} -> {ch.Value}");
+            }
+        }
+    }
+}
