c30141d1a87209f4683bb7baa006c26fe3ba3d59
Nikolay Sinorov <nsinorov123@gmail.com>
2022-11-17 13:29:07 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Text Processing - Lab/Text Filter/Text Filter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Text_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] keyWord = Console.ReadLine().Split(", ");
+
+            string text = Console.ReadLine();
+
+            foreach (var currBanWord in keyWord)
+            {
+                text = text.Replace(currBanWord, new string('*', currBanWord.Length));
+                
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
