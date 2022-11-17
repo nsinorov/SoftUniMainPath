7f654ce7ed84478426c61b307e176152e194d1c1
Nikolay Sinorov <nsinorov123@gmail.com>
2022-11-17 13:27:38 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Text Processing - Lab/Repeat Strings/Repeat Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Repeat_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+
+            string result = string.Empty;
+
+            foreach (var word in words)
+            {           
+                for (int i = 0; i < word.Length; i++)
+                {
+                    result += word;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
