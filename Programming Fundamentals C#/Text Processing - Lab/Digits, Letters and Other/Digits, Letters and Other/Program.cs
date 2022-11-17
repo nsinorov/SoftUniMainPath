18969170f935c71cb894f2348ebb6e7884c529cd
Nikolay Sinorov <nsinorov123@gmail.com>
2022-11-17 13:27:13 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Text Processing - Lab/Digits, Letters and Other/Digits, Letters and Other/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Digits__Letters_and_Other
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var nums = new StringBuilder();
+            var letters = new StringBuilder();
+            var chars = new StringBuilder();
+
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var chartext = input[i];
+
+                if (char.IsDigit(chartext))
+                {
+                    nums.Append(chartext);
+                }
+                else if (char.IsLetter(chartext))
+                {
+                    letters.Append(chartext);
+                }
+                else
+                {
+                    chars.Append(chartext);
+                }
+            }
+            Console.WriteLine($"{nums}\n{letters}\n{chars}");     
+        }
+    }
+}
