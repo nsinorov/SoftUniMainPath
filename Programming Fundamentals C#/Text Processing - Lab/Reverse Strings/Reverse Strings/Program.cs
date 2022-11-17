d5a59ca352e9f8917882e7fbb9540c59cc8d4f3d
Nikolay Sinorov <nsinorov123@gmail.com>
2022-11-17 13:28:05 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Text Processing - Lab/Reverse Strings/Reverse Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+           
+            while (input != "end")
+            {
+                char[] chars = input.ToCharArray();
+                Array.Reverse(chars);
+                string reversedString = new string(chars);
+                Console.WriteLine($"{input} = {reversedString}");
+                input = Console.ReadLine();
+            }
+           
+        }
+    }
+}
