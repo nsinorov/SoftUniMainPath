c2ff5d86963c9b00429100859725e2eb42c10cc2
Nikolay Sinorov <nsinorov123@gmail.com>
2022-11-17 13:28:40 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Text Processing - Lab/Substring/Substring/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {        
+            string specialWord = Console.ReadLine();
+
+            string text = Console.ReadLine();
+
+            while (text.Contains(specialWord))
+                
+            {
+                int startIndex = text.IndexOf(specialWord);
+
+                text = text.Remove(startIndex, specialWord.Length);
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
