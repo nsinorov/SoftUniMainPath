33f395947fccd3cb885d0f4ab1fb28cf64e799d7
Nikolay Sinorov <nsinorov123@gmail.com>
2022-11-20 21:21:54 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Text Processing - Exercise/Extract File/Extract File/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Extract_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split("\\");
+
+            string file = input[input.Length - 1];
+            string[] splitLastWord = file.Split(".");
+            string fileName = splitLastWord[0];
+            string extension = splitLastWord[1];
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {extension}");
+        }
+    }
+}
