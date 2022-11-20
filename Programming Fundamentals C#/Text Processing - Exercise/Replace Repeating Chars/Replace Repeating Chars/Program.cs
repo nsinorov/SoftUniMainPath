2a6262ccb310ab0f6ab729e909407db7a1ffb941
Nikolay Sinorov <nsinorov123@gmail.com>
2022-11-20 21:20:35 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Text Processing - Exercise/Replace Repeating Chars/Replace Repeating Chars/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Replace_Repeating_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            char previousChar = text[0];
+            Console.Write(previousChar);
+
+            for (int i = 1; i < text.Length; i++)
+            {
+                char currChar = text[i];
+
+                if(previousChar != currChar)
+                {
+                    previousChar = currChar;
+                    Console.Write(previousChar);
+                }
+            }
+        }
+    }
+}
