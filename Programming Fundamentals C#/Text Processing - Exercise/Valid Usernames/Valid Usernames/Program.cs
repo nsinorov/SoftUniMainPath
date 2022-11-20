69bd6f1c5ccf64c2d5ffc096d573222f0c93ee50
Nikolay Sinorov <nsinorov123@gmail.com>
2022-11-20 21:19:43 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Text Processing - Exercise/Valid Usernames/Valid Usernames/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Valid_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine().Split(", ");
+
+            foreach (string currName in usernames)
+            {
+                if (currName.Length > 3 && currName.Length <= 16)
+                {
+                    bool isUsernameValid = true;
+
+                    foreach (char currChar in currName)
+                    {
+                        if (!(char.IsLetterOrDigit(currChar) || currChar == '-' || currChar == '_'))
+                        {
+                            isUsernameValid = false;
+                            break;
+                        }
+                    }
+                    if (isUsernameValid)
+                    {
+                        Console.WriteLine(currName);
+                    }
+                }
+            }
+        }
+    }
+}
