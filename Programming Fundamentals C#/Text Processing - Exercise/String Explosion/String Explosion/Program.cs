9c77e284c404a6ceceab2516fb195a7eed1b74d4
Nikolay Sinorov <nsinorov123@gmail.com>
2022-11-20 21:20:09 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Text Processing - Exercise/String Explosion/String Explosion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace T07._String_Explosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string textField = Console.ReadLine();
+
+            int bomb = 0;
+
+            for (int i = 0; i < textField.Length; i++)
+            {           
+                if (bomb > 0 && textField[i] != '>')
+                {
+                    textField = textField.Remove(i, 1);
+                    bomb--;
+                    i--;
+                }             
+                else if (textField[i] == '>')
+                {
+                    bomb += int.Parse(textField[i + 1].ToString());
+                }
+            }
+            Console.WriteLine(textField);
+        }
+    }
+}
