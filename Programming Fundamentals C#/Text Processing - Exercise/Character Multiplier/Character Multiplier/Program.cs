e9babc81b89044b2b45723a45503212a6d97a92d
Nikolay Sinorov <nsinorov123@gmail.com>
2022-11-20 21:22:26 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Text Processing - Exercise/Character Multiplier/Character Multiplier/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Character_Multiplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            GetStringSum(input[0], input[1]);
+        }
+        private static void GetStringSum(string stringOne, string stringTwo)
+        {
+            int sum = 0;
+            int minLenght = Math.Min(stringOne.Length, stringTwo.Length);
+            for (int i = 0; i < minLenght; i++)
+            {
+                sum += stringOne[i] * stringTwo[i];
+            }
+
+            string longestString = stringOne;
+            if (longestString.Length < stringTwo.Length)
+            {
+                longestString = stringTwo;
+            }
+            for (int i = minLenght; i < longestString.Length; i++)
+            {
+                sum += longestString[i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
