994bf448e4216829a436fef53ab67bad770da23f
Nikolay Sinorov <nsinorov123@gmail.com>
2022-11-20 21:22:53 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Text Processing - Exercise/Caesar Cipher/Caesar Cipher/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Caesar_Cipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string encrypted = string.Empty;
+
+            foreach (char currChar in input)
+            {
+                int intPosition = currChar;
+                intPosition += 3;
+                encrypted += (char)intPosition;
+            }
+            Console.WriteLine(encrypted);
+        }
+    }
+}
