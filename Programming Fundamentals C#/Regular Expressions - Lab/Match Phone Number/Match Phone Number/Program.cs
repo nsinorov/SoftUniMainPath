c3b6206b648c90fc1947a30f03d066c91eded9d4
Nikolay Sinorov <nsinorov123@gmail.com>
2022-11-29 21:35:44 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Regular Expressions - Lab/Match Phone Number/Match Phone Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Match_Phone_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\+\b359(-| )2\1\d{3}\1\d{4}\b";
+
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex(pattern);
+            MatchCollection matchCollection = regex.Matches(input);
+
+            string[] result = matchCollection.Select(x => x.Value).ToArray();
+
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
