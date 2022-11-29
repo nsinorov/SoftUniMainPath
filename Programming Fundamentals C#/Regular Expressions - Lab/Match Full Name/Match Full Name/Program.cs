054d17d49d76c12f24db5489f398d91883d0f13a
Nikolay Sinorov <nsinorov123@gmail.com>
2022-11-29 21:34:42 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Regular Expressions - Lab/Match Full Name/Match Full Name/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Match_Full_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(input);
+
+            foreach (Match item in matches)
+            {
+                Console.Write(item.Value + " ");
+            }
+        }
+    }
+}
