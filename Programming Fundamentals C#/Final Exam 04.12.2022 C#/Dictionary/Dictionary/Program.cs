5bf1b77f4dca50ea7ad93d751723907177862f84
Nikolay Sinorov <nsinorov123@gmail.com>
2022-12-07 08:38:13 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Final Exam 04.12.2022 C#/Dictionary/Dictionary/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                List<string> List1 = Console.ReadLine().Split(" | ").ToList();
+                var wordDataBase = new Dictionary<string, List<string>>();
+                List<string> word = Console.ReadLine().Split(" | ").ToList();
+                string inputCommand = Console.ReadLine();
+                List<string> alpabet = new List<string>();
+                int b;
+                for (int i = 0; i < List1.Count; i++)
+                {
+
+                    alpabet = List1[i].Split(": ").ToList();
+                    if (wordDataBase.ContainsKey(alpabet[0]) == false)
+                    {
+                        wordDataBase.Add(alpabet[0], new List<string>());
+                    }
+                    for (b = 1; b < alpabet.Count; b++)
+                    {
+
+                        wordDataBase[alpabet[0]].Add(alpabet[b]);
+                    }
+
+                    alpabet.Clear();
+                }
+
+                if (inputCommand == "Test")
+                {
+                    foreach (var item in word)
+                    {
+                        if (wordDataBase.ContainsKey(item))
+                        {
+                            Console.WriteLine($"{item}:");
+                            var li = wordDataBase[item];
+                            foreach (var wo in li)
+                            {
+                                Console.WriteLine($" -{wo}");
+                            }
+                        }
+                    }
+                }
+                else if (inputCommand == "Hand Over")
+                {
+                    var keys = wordDataBase.Keys;
+                    Console.WriteLine(string.Join(' ', keys));
+                }
+            }
+        }
+    }
+}
