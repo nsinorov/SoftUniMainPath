5bf9466a2b769a06d808b62a5c20a494486832af
Nikolay Sinorov <nsinorov123@gmail.com>
2022-12-29 01:53:08 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Bitwise Operations/P-th Bit/P-th Bit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace P_th_Bit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int position = int.Parse(Console.ReadLine());
+
+            int mask = 1 << position;//after shift left with position value we have 1 value in mask at current position but another are 0
+
+            int result = (number & mask) >> position;//after shift right with position value we have 0 or 1 at last position but another are 0 using &
+
+            Console.WriteLine(result);
+        }
+    }
+}
