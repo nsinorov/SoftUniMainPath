16095decf954427e3119d643f887e7d552bdcbfc
Nikolay Sinorov <nsinorov123@gmail.com>
2022-12-29 01:48:23 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Bitwise Operations/Bit at Position 1/Bit at Position 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Bit_at_Position_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {       
+            int num = int.Parse(Console.ReadLine());
+
+            int mask = 1 << 1;//after shift left with 1 we have 1 value in mask at position 1 another are 0
+
+            int result = (num & mask) >> 1;//after shift right with 1 taken last value(0 or 1) because another are 0 using &
+
+            Console.WriteLine(result);
+        }
+    }
+}
