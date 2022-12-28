bc9b00f834794700ad01b7cc2deee85c21b5b8ef
Nikolay Sinorov <nsinorov123@gmail.com>
2022-12-29 01:57:08 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Bitwise Operations/Bit Destroyer/Bit Destroyer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Bit_Destroyer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int position = int.Parse(Console.ReadLine());
+
+
+            int mask = ~(1 << position);//after shift left with position we have mask with 1 value at current position and another are 0, but with ~(not) we have mask with 0 value at current position and another are 1
+
+            int result = (number & mask);// taken the whole number with set 0 only at current position and now we have another number
+
+            Console.WriteLine(result);
+        }
+    }
+}
