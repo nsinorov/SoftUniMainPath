c39e8d19a9191f8600cdca0a4d523ccd9b699c02
Nikolay Sinorov <nsinorov123@gmail.com>
2022-12-29 00:05:44 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Bitwise Operations/Binary Digits Count/Binary Digits Count/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Binary_Digits_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int zeroOrOne = int.Parse(Console.ReadLine()); // 0 or 1
+
+            int counter = 0; // how many zero's or one's
+
+            while (num > 0)
+            {
+
+                int leftOver = num % 2;
+
+                if (leftOver == zeroOrOne)
+                {
+                    counter++;
+                }
+
+                num /= 2;
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
