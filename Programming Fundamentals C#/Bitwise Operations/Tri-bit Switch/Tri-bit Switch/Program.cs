5065c0a989d54bb6efbddae39a4f3230a4b95b32
Nikolay Sinorov <nsinorov123@gmail.com>
2022-12-29 02:11:12 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Bitwise Operations/Tri-bit Switch/Tri-bit Switch/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Tri_bit_Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int position = int.Parse(Console.ReadLine());
+
+            int mask = 7 << position;// we need 111 at current position and int 7 = 111
+
+            int result = number ^ mask;//1 ^ 1 = 0, 0 ^ 1 = 1
+
+            Console.WriteLine(result);
+        }
+    }
+}
