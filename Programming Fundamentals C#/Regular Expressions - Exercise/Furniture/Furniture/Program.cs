fe5c208b3f181edab70c58097e991c7ba8607eaa
Nikolay Sinorov <nsinorov123@gmail.com>
2022-12-29 17:57:19 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Regular Expressions - Exercise/Furniture/Furniture/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Regular_ExpressionsEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> finalFurnitures = new List<string>();
+            double totalSum = 0;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "Purchase")
+                {
+                    break;
+                }
+
+                string pattern = @">>(?<furniture>[A-Za-z]+)<<(?<price>\d+(.\d+)?)!(?<amount>\d+)";
+
+
+
+                Match furnitures = Regex.Match(command, pattern, RegexOptions.IgnoreCase);
+
+                var nameFurniture = furnitures.Groups["furniture"].Value;
+                var priceFurniture = furnitures.Groups["price"].Value;
+                var amountFurniture = furnitures.Groups["amount"].Value;
+
+                if (furnitures.Success)
+                {
+                    totalSum += double.Parse(priceFurniture) * double.Parse(amountFurniture);
+                    finalFurnitures.Add(nameFurniture);
+                }
+
+            }
+
+            Console.WriteLine("Bought furniture:");
+
+            for (int i = 0; i < finalFurnitures.Count; i++)
+            {
+                Console.WriteLine(finalFurnitures[i]);
+            }
+
+            Console.WriteLine($"Total money spend: {totalSum:f2}");
+        }
+    }
+}
