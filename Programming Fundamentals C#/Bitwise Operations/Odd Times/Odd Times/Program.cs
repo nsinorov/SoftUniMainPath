bdff65cb448fdacb30513c2d0d14e5e4d880713f
Nikolay Sinorov <nsinorov123@gmail.com>
2022-12-29 02:04:03 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Bitwise Operations/Odd Times/Odd Times/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Odd_Times
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int result = numbers[0];
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                result ^= numbers[i];
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
