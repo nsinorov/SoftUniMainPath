bcf8e918df62a55eb87c2f7874d34b920d70b2d9
Nikolay Sinorov <nsinorov123@gmail.com>
2022-12-29 17:58:16 +0200

    Add files via upload

---
+++ Programming Fundamentals C#/Regular Expressions - Exercise/SoftUni Bar Income/SoftUni Bar Income/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"^%(?<customer>[A-Z][a-z]+)%[^|$%.]*<(?<product>\w+)>[^|$%.]*\|(?<count>\d+)\|[^|$%.]*?(?<price>[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?)\$";
+            double totalIncome = 0;
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+
+                if (line == "end of shift")
+                {
+                    break;
+                }
+
+                if (Regex.IsMatch(line, pattern))
+                {
+                    Match match = Regex.Match(line, pattern);
+                    var customer = match.Groups["customer"].Value;
+                    string product = match.Groups["product"].Value;
+                    int count = int.Parse(match.Groups["count"].Value);
+                    double price = double.Parse(match.Groups["price"].Value);
+                    double money = price * count;
+                    Console.WriteLine($"{customer}: {product} - {money:F2}");
+                    totalIncome += money;
+                }
+            }
+            Console.WriteLine($"Total income: {totalIncome:F2}");
+        }
+    }
+}
