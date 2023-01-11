ffc02535a924b3d0c5f1a482c670a430a77cb245
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-11 02:35:16 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Stacks and Queues - Lab/Reverse a String/Reverse a String/Program.cs
@@ -0,0 +1,12 @@
+﻿string input = Console.ReadLine();
+var stack = new Stack<char>();
+
+foreach (var character in input)
+{
+    stack.Push(character);
+}
+
+while (stack.Count != 0)
+{
+    Console.Write(stack.Pop());
+}
