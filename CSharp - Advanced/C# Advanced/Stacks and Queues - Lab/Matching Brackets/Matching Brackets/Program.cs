22ca5249c0d660e12d6793cd923a407fe715fa42
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-12 22:10:49 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Stacks and Queues - Lab/Matching Brackets/Matching Brackets/Program.cs
@@ -0,0 +1,25 @@
+﻿string input = Console.ReadLine();
+
+Stack<int> stack = new Stack<int>();
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i] == '(')
+    {
+        stack.Push(i);
+    }
+    if (input[i] == ')')
+    {
+        int openBracket = stack.Pop();
+
+        for (int j = openBracket; j <= i; j++)
+        {
+            if (input[j] == ' ')
+            {
+                continue;
+            }
+            Console.Write(input[j]);
+        }
+        Console.WriteLine();
+    }
+}
