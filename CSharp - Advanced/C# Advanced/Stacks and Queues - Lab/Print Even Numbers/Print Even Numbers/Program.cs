561f66ef810f22fa6704b54d1cd3eeeadba4eef6
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-13 17:18:19 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Stacks and Queues - Lab/Print Even Numbers/Print Even Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿
+int[] arrayOfNums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+Queue<int> queue = new Queue<int>();
+
+foreach (int num in arrayOfNums)
+{
+    queue.Enqueue(num);
+}
+
+bool first = true;
+
+while (queue.Count > 0)
+{
+    int num = queue.Dequeue();
+
+    if (num % 2 == 0)
+    {
+        if (!first)
+        {
+            Console.Write(", ");
+        }
+        Console.Write(num);
+        first = false;
+    }
+}
+
+ 
