e84b77e86d7ca2bd46f4606bbee8e66d660649bb
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-13 15:49:06 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Stacks and Queues - Lab/Hot Potato/Hot Potato/Program.cs
@@ -0,0 +1,21 @@
+﻿string[] children = Console.ReadLine().Split();
+Queue<string> queue = new Queue<string>(children);
+int tosses = int.Parse(Console.ReadLine());
+int startToss = 1;
+
+while (queue.Count != 1)
+{
+    string child = queue.Dequeue();
+    if (startToss < tosses)
+    {
+        startToss++;
+        queue.Enqueue(child);
+
+    }
+    else
+    {
+        Console.WriteLine($"Removed {child}");
+        startToss = 1;
+    }
+}
+Console.WriteLine($"Last is {queue.Dequeue()}");
