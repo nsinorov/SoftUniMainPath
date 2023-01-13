ccadb04f884fa9cc2552cd8496f830d13d5f9565
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-13 15:49:40 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Stacks and Queues - Lab/Traffic Jam/Traffic Jam/Program.cs
@@ -0,0 +1,27 @@
+﻿int numOfCars = int.Parse(Console.ReadLine());
+string command = Console.ReadLine();
+Queue<string> queue = new Queue<string>();
+int count = 0;
+
+while(command != "end")
+{
+    if(command == "green")
+    {
+        for (int i = 0; i < numOfCars; i++)
+        {
+            // check if there are cars
+            if(queue.Count == 0)
+            {
+                break;
+            }
+            count++;
+            Console.WriteLine($"{queue.Dequeue()} passed!");
+        }
+    }
+    else
+    {
+        queue.Enqueue(command);
+    }
+    command = Console.ReadLine();
+}
+Console.WriteLine($"{count} cars passed the crossroads.");
