83f8016135412bb0108df459e195cdc056a8d1e9
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-13 17:36:11 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Stacks and Queues - Lab/Simple Calculator/Simple Calculator/Program.cs
@@ -0,0 +1,27 @@
+﻿// Split the input expression by space to extract its tokens
+string[] tokens = Console.ReadLine().Split(' ').ToArray();
+// Reverse the input tokens
+Array.Reverse(tokens);
+// Push the tokens in a stack
+var stack = new Stack<string>(tokens);
+// Initialize the current result as the first number in the stack
+int result = int.Parse(stack.Pop());
+// Repeat the following steps until the stack gets empty
+while (stack.Count > 0)
+{
+    // Pop an operation and number
+    string op = stack.Pop();
+    int num = int.Parse(stack.Pop());
+
+    // Execute the operation
+    if (op == "+")
+    {
+        result += num;
+    }
+    else if (op == "-")
+    {
+        result -= num;
+    }
+}
+// result
+Console.WriteLine(result);
