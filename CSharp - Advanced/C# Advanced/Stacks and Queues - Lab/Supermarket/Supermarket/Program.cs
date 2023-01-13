f8ccd6487b656f857b4557d3c76c2a245db3f8c0
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-13 09:03:03 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Stacks and Queues - Lab/Supermarket/Supermarket/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Queue<string> queue = new Queue<string>();
+
+while (true)
+{
+    string input = Console.ReadLine();
+
+    if (input == "Paid")
+    {      
+        while (queue.Any())
+        {
+            Console.WriteLine(queue.Dequeue());
+        }
+    }
+    else if (input == "End")
+    {
+        Console.WriteLine($"{queue.Count} people remaining.");
+        break;
+    }
+    else
+    {
+        queue.Enqueue(input);
+    }
+}
