1a064ebde1a743e404aaad53c63150f0e0c07e94
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-14 21:36:44 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Stacks and Queues - Exercises/Basic Stack Operations/Basic Stack Operations/Program.cs
@@ -0,0 +1,41 @@
+﻿int[] tokens = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int[] nums = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+var stack = new Stack<int>();
+
+int pushElement = tokens[0];
+int popElement = tokens[1];
+int checkNum = tokens[2];
+
+for (int i = 0; i < pushElement; i++)
+{
+    stack.Push(nums[i]);
+}
+
+for (int i = 0; i < popElement; i++)
+{
+    stack.Pop();
+}
+
+if (stack.Contains(checkNum))
+{
+    Console.WriteLine("true");
+}
+else
+{
+    if (stack.Any())
+    {
+        Console.WriteLine(stack.Min());
+    }
+    else
+    {
+        Console.WriteLine(0);
+    }
+}
