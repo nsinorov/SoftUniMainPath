4c2b0c74d5b9588b53cb0c4bf407f62213cbb469
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-14 21:37:11 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Stacks and Queues - Exercises/Basic Queue Operations/Basic Queue Operations/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] tokens = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int[] nums = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+var queue = new Queue<int>(nums);
+
+int elementsToDeque = tokens[1];
+int number = tokens[2];
+
+for (int i = 0; i < elementsToDeque; i++)
+{
+    queue.Dequeue();
+}
+
+if (queue.Contains(number))
+{
+    Console.WriteLine("true");
+}
+else
+{
+    if (queue.Any())
+    {
+        Console.WriteLine(queue.Min());
+    }
+    else
+    {
+        Console.WriteLine(0);
+    }
+}
