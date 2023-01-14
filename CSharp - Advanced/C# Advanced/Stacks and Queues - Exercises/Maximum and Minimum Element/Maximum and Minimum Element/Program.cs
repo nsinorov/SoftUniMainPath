7a6d6cc76c507718bb7645e91e153e67baa07025
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-14 21:40:33 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Stacks and Queues - Exercises/Maximum and Minimum Element/Maximum and Minimum Element/Program.cs
@@ -0,0 +1,35 @@
+﻿int query = int.Parse(Console.ReadLine());
+
+var stack = new Stack<int>();
+
+for (int i = 0; i < query; i++)
+{
+    string[] tokens = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    int command = int.Parse(tokens[0]);
+
+    switch (command)
+    {
+        case 1:
+            int number = int.Parse(tokens[1]);
+            stack.Push(number);
+            break;      
+        case 2:
+            stack.Pop();
+            break;
+        case 3:
+            if (stack.Any())
+            {
+                Console.WriteLine(stack.Max());
+            }
+            break;
+        case 4:
+            if (stack.Any())
+            {
+                Console.WriteLine(stack.Min());
+            }
+            break;
+    }
+}
+Console.WriteLine(string.Join(", ", stack));
