8ed01abf61b81a2c8f524aaa13a1c26951ad9f0c
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-14 21:43:08 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Stacks and Queues - Exercises/Fast Food/Fast Food/Program.cs
@@ -0,0 +1,29 @@
+﻿int quantityFood = int.Parse(Console.ReadLine());
+
+Queue<int> queue = new(
+    Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse));
+
+Console.WriteLine(queue.Max());
+
+while (queue.Any())
+{
+    quantityFood -= queue.Peek();
+
+    if (quantityFood < 0)
+    {
+        break;
+    }
+    queue.Dequeue();
+
+}
+
+if (queue.Any())
+{
+    Console.WriteLine($"Orders left: {string.Join(" ", queue)}");
+}
+else
+{
+    Console.WriteLine("Orders complete");
+}
