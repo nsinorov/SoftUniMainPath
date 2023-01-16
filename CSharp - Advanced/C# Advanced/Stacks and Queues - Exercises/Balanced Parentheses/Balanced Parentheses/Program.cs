917ec9fc49d38f8629478b10d9cd564d5a7876de
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-16 20:20:34 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Stacks and Queues - Exercises/Balanced Parentheses/Balanced Parentheses/Program.cs
@@ -0,0 +1,48 @@
+﻿string parentheses = Console.ReadLine();
+
+var stack = new Stack<char>();
+
+foreach (var parenthes in parentheses)
+{
+    switch (parenthes)
+    {
+        case '(':      
+        case '{':     
+        case '[':
+            stack.Push(parenthes); // pushing the opening brackets
+            break;
+
+        case '}':
+            if(stack.Count == 0 || stack.Pop() != '{')
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+            break;
+
+        case ')':
+            if (stack.Count == 0 || stack.Pop() != '(')
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+            break;
+
+        case ']':
+            if (stack.Count == 0 || stack.Pop() != '[')
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+            break;
+    }
+}
+
+if (stack.Count > 0)
+{
+    Console.WriteLine("NO");
+}
+else
+{
+    Console.WriteLine("YES");
+}
