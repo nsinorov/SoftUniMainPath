1d6456c6060c2038ec7fab11e418e261edde135a
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-16 20:24:25 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Stacks and Queues - Exercises/Fashion Boutique/Fashion Boutique/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+Stack<int> clothesInBox = new(
+    Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse));
+
+int capacityOfRack = int.Parse(Console.ReadLine());
+
+int currentRack = capacityOfRack;
+int numberOfRacks = 1;
+
+while (clothesInBox.Any())
+{
+    currentRack -= clothesInBox.Peek();
+
+    if(currentRack < 0)
+    {
+        currentRack = capacityOfRack;
+        numberOfRacks++;
+
+        continue;
+    }
+    clothesInBox.Pop();  
+}
+Console.WriteLine(numberOfRacks);
