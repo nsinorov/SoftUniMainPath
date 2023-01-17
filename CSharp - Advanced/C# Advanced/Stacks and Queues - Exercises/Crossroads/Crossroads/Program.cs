5fa63854be5231448486447a95474cca28c6e7a9
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-17 23:02:47 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Stacks and Queues - Exercises/Crossroads/Crossroads/Program.cs
@@ -0,0 +1,63 @@
+﻿int durationOfGreenLight = int.Parse(Console.ReadLine());
+int freeWindowSec = int.Parse(Console.ReadLine());
+
+string command = Console.ReadLine();
+
+int passedCarsCounter = 0;
+
+var queue = new Queue<string>();
+
+while (command != "END")
+{
+    if (command == "green")
+    {
+        int greenLight = durationOfGreenLight;
+        int freeWindow = freeWindowSec;
+
+        while (queue.Any())
+        {
+            string currentCar = queue.Peek();
+
+            if (greenLight >= currentCar.Length)
+            {
+                greenLight -= currentCar.Length;
+                queue.Dequeue();
+                passedCarsCounter++;
+            }
+            else if (greenLight <= 0)
+            {
+                break;
+            }
+            else if (greenLight < currentCar.Length)
+            {
+                if (greenLight + freeWindow >= currentCar.Length)
+                {
+                    greenLight = 0;
+                    freeWindow -= currentCar.Length;
+                    queue.Dequeue();
+                    passedCarsCounter++;
+                }
+                else
+                {
+                    int indexOfCrash = greenLight + freeWindow;
+
+                    if (indexOfCrash >= 0 && indexOfCrash < currentCar.Length)
+                    {
+                        Console.WriteLine("A crash happened!");
+                        Console.WriteLine($"{currentCar} was hit at {currentCar[indexOfCrash]}.");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+    else
+    {
+        queue.Enqueue(command);
+    }
+
+    command = Console.ReadLine();
+}
+
+Console.WriteLine("Everyone is safe.");
+Console.WriteLine($"{passedCarsCounter} total cars passed the crossroads.");
