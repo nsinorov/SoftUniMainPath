04989422583e8968914d4f2bf6d59cca7c62a3b9
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-18 22:45:49 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Stacks and Queues - Exercises/Truck Tour/Truck Tour/Program.cs
@@ -0,0 +1,50 @@
+﻿const int litersPerKilometer = 1;
+int numOfPetrolPumps = int.Parse(Console.ReadLine());
+
+Queue<int[]> pumps = new();
+
+for (int i = 0; i < numOfPetrolPumps; i++)
+{
+    int[] information = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray();
+
+    pumps.Enqueue(information);
+}
+
+int startIndex = 0;
+
+while (true)
+{
+    bool isComplete = true;
+    int totalLiters = 0;
+
+    foreach (var pump in pumps)
+    {
+        int liters = pump[0];
+        int distance = pump[1];
+
+        totalLiters += liters;
+
+        if (totalLiters - distance * litersPerKilometer < 0)
+        {
+            startIndex++;
+
+            int[] currentPump = pumps.Dequeue();
+            pumps.Enqueue(currentPump);
+
+            isComplete = false;
+
+            break;
+        }
+
+        totalLiters -= distance * litersPerKilometer;
+    }
+
+    if (isComplete)
+    {
+        Console.WriteLine(startIndex);
+        break;
+    }
+}
