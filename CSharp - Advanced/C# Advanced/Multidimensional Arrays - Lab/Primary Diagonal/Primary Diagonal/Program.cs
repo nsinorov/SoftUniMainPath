a30f118369a3a047c6ed147ecfad79c4f7589379
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-19 23:44:52 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Multidimensional Arrays - Lab/Primary Diagonal/Primary Diagonal/Program.cs
@@ -0,0 +1,22 @@
+﻿int sizeOfMatrix = int.Parse(Console.ReadLine());
+
+int primaryDiagonal = 0;
+
+int[,] square = new int[sizeOfMatrix, sizeOfMatrix];
+
+for (int row = 0; row < sizeOfMatrix; row++)
+{
+    int[] values = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+	for (int col = 0; col < sizeOfMatrix; col++)
+	{
+        square[row, col] = values[col];
+    }
+}
+
+for (int i = 0; i < sizeOfMatrix; i++)
+{
+    primaryDiagonal += square[i, i];
+}
+
+Console.WriteLine(primaryDiagonal);
