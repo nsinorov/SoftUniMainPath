73b8864158188bd9db44a5ad37065494062e533c
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-20 23:18:52 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Multidimensional Arrays - Lab/Sum Matrix Elements/Sum Matrix Elements/Program.cs
@@ -0,0 +1,37 @@
+﻿
+int[] dimensions = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse).
+    ToArray();
+
+int rows = dimensions[0];
+int cols = dimensions[1];
+
+int[,] matrix = new int[rows, cols];
+
+for (int row = 0; row < rows; row++)
+{
+    int[] nums = Console.ReadLine()
+        .Split(", ",StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse).
+        ToArray();
+
+    for (int col = 0; col < cols; col++)
+    {
+        matrix[row, col] = nums[col];
+    }
+}
+
+int sum = 0;
+
+for (int row = 0; row < rows; row++)
+{
+    for (int col = 0; col < cols; col++)
+    {
+        sum += matrix[row, col];
+    }
+}
+
+Console.WriteLine(dimensions[0]);
+Console.WriteLine(dimensions[1]);
+Console.WriteLine(sum);
