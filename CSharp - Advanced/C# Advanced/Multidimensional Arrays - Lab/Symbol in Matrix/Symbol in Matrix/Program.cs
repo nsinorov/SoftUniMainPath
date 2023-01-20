bf42b041810cf0b48d5bab804f50762814c23da2
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-20 23:21:59 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Multidimensional Arrays - Lab/Symbol in Matrix/Symbol in Matrix/Program.cs
@@ -0,0 +1,28 @@
+﻿int sizeOfMatrix = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[sizeOfMatrix, sizeOfMatrix];
+
+for (int row = 0; row < sizeOfMatrix; row++)
+{
+    string input = Console.ReadLine();
+
+    for (int col = 0; col < sizeOfMatrix; col++)
+    {
+        matrix[row, col] = input[col];
+    }
+}
+
+char symbol = char.Parse(Console.ReadLine());
+
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    for (int col = 0; col < matrix.GetLength(1); col++)
+    {
+        if (matrix[row, col] == symbol)
+        {
+            Console.WriteLine($"({row}, {col})");
+            return;
+        }
+    }
+}
+Console.WriteLine($"{symbol} does not occur in the matrix");
