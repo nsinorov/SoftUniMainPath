3b6b554654526ac41ee9095de1d212bc281ff632
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-21 14:41:23 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Multidimensional Arrays - Exercise/Diagonal Difference/Diagonal Difference/Program.cs
@@ -0,0 +1,28 @@
+﻿int size = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[size, size];
+
+for (int row = 0; row < size; row++)
+{
+    int[] numbers = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray();
+
+    for (int col = 0; col < size; col++)
+    {
+        matrix[row, col] = numbers[col];
+    }
+
+}
+
+int primary = 0;
+int secondary = 0;
+
+for (int i = 0; i < size; i++)
+{
+    primary += matrix[i, i];
+    secondary += matrix[size - 1 - i, i];
+}
+
+Console.WriteLine(Math.Abs(primary - secondary));
