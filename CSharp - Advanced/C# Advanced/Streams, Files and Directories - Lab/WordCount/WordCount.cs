53810ebe5a95bedc0f83b6d659641941ac7cf06e
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-26 22:11:29 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Streams, Files and Directories - Lab/WordCount/WordCount.cs
@@ -0,0 +1,61 @@
+﻿namespace WordCount
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class WordCount
+    {
+        static void Main()
+        {
+
+            string wordPath = @"..\..\..\Files\words.txt";
+            string textPath = @"..\..\..\Files\text.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            CalculateWordCounts(wordPath, textPath, outputPath);
+        }
+
+        public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
+        {
+            Dictionary<string, int> words = new Dictionary<string, int>();
+
+            using StreamReader readerOne = new StreamReader(textFilePath);
+            {
+                using StreamReader readerTwo = new StreamReader(wordsFilePath);
+                {
+                    using StreamWriter writer = new StreamWriter(outputFilePath);
+                    {
+                        string[] wordsList = readerTwo.ReadLine().Split();
+
+                        while (!readerOne.EndOfStream)
+                        {
+                            var line = readerOne.ReadLine().ToLower();
+
+                            for (int i = 0; i < wordsList.Length; i++)
+                            {
+                                var word = wordsList[i].ToString().ToLower();
+
+                                if (line.Contains(word))
+                                {
+                                    if (!words.ContainsKey(word))
+                                    {
+                                        words[word] = 0;
+                                    }
+                                    words[word]++;
+                                }
+                            }
+                        }
+                        words = words.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+                        foreach (var word in words)
+                        {
+                            writer.WriteLine($"{word.Key} - {word.Value}");
+                        }
+                    }
+                }              
+            }                
+        }
+    }
+}
