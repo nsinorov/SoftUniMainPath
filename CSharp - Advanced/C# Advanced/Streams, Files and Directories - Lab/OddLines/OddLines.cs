19e8ea54d40a07ba2131b69f9d92811b29019eb8
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-26 22:06:51 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Streams, Files and Directories - Lab/OddLines/OddLines.cs
@@ -0,0 +1,38 @@
+﻿namespace OddLines
+{
+    using System.IO;
+	
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            using(StreamReader reader = new StreamReader(inputFilePath))
+            {
+                using(StreamWriter writer = new StreamWriter(outputFilePath))
+                {
+                   string line = string.Empty;
+                    int row = 1;
+
+                    while(line != null)
+                    {
+                        line = reader.ReadLine();
+
+                        if(row % 2 == 0)
+                        {
+                            writer.WriteLine(line);
+                        }
+                        row++;
+                    }
+                }
+            }
+        }
+    }
+}
