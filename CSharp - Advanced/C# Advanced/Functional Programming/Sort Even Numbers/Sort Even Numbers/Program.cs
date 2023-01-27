ae832b01404e81e4cf0f38df1fbfd88ec00be71e
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-28 01:03:38 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Functional Programming/Sort Even Numbers/Sort Even Numbers/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] nums = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .Where(n => n % 2 == 0)
+    .OrderBy(n => n)
+    .ToArray();
+
+Console.WriteLine(string.Join(", ", nums));
