4e2af8b8d0c16dc92c7633df36d92ebf5f9b720b
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-27 23:09:48 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Streams, Files and Directories - Exercises/LineNumbers/LineNumbers.cs
@@ -0,0 +1,38 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+
+            StringBuilder sb = new();
+
+            string[] lines = File.ReadAllLines(inputFilePath);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                int letterCount = lines[i].Count(char.IsLetter);
+                int punctuationCount = lines[i].Count(char.IsPunctuation);
+
+                sb.AppendLine($"Line {i + 1}: {lines[i]} ({letterCount})({punctuationCount})");
+            }
+
+            File.WriteAllText(outputFilePath, sb.ToString());
+
+
+        }
+    }
+}
