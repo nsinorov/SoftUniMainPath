5ac70418a90b5d4c12e71a6857b6ed9adfd42da9
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-27 23:08:09 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Streams, Files and Directories - Exercises/EvenLines/EvenLines.cs
@@ -0,0 +1,48 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            string result = string.Empty;
+
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                int lineNumber = 0;
+
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    if (lineNumber % 2 == 0)
+                    {
+                        line = Regex.Replace(line, "[-.!?,]", "@");
+
+                        string[] words = line.Split(' ');
+
+                        Array.Reverse(words);
+
+                        line = string.Join(" ", words);
+
+                        result += line + "\n";                     
+                    }
+                    lineNumber++;
+                }
+
+            }
+            return result;
+        }
+    }
+}
+
