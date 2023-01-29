7a0db252e4fb4403b2c2421071036238b39af3bf
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-29 22:07:29 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Functional Programming/Count Uppercase Words/Count Uppercase Words/Program.cs
@@ -0,0 +1,9 @@
+﻿Predicate<string> filterUpLetters = 
+    f => f[0] == char.ToUpper(f[0]) && char.IsLetter(f[0]);
+
+string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Where(f => filterUpLetters(f)).ToArray();
+
+foreach (var word in input)
+{
+    Console.WriteLine(word);
+}
