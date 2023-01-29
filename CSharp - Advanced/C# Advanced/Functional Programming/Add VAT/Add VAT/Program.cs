ccb7ee7baf7032366d69562b2dff39d990949301
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-29 22:09:32 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Functional Programming/Add VAT/Add VAT/Program.cs
@@ -0,0 +1,9 @@
+﻿double[] prices = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(double.Parse)
+    .ToArray();
+
+foreach (var nums in prices)
+{
+    Console.WriteLine($"{nums * 1.2 :f2}");
+}
