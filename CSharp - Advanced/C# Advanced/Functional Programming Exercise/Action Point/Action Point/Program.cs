1bcc3c073a472abe68b242725c03cb77d9cf3e07
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-29 22:13:23 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Functional Programming Exercise/Action Point/Action Point/Program.cs
@@ -0,0 +1,6 @@
+﻿Action<string[]> print = strings
+    => Console.WriteLine(string.Join(Environment.NewLine, strings));
+
+string[] name = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+print(name);
