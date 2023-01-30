08bccb710841fc9aac1612c233b441865e9ec630
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-30 23:39:01 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Functional Programming Exercise/Knights of Honor/Knights of Honor/Program.cs
@@ -0,0 +1,12 @@
+﻿Action<string[], string> printNamesWithAddedTitle = (names, title) =>
+{
+    foreach (var name in names)
+    {
+        Console.WriteLine($"{title} {name}");
+    }
+};
+
+string[] names = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+printNamesWithAddedTitle(names, "Sir");
