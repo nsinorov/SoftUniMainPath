d82d036452a59950d811e6cfbe67f0eca4409212
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-30 23:35:12 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Functional Programming Exercise/Custom Min Function/Custom Min Function/Program.cs
@@ -0,0 +1,18 @@
+﻿Func<int[], int> min = (nums) =>
+{
+	int min = int.MaxValue;
+
+	foreach (var num in nums)
+	{
+		if (num < min)
+		{
+			min = num;
+		}
+	}
+	return min;
+};
+
+
+int[] nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+Console.WriteLine(min(nums));
