83467eb5b58f874d93ac5278ced21cbdc5db6cbd
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-01 12:00:52 -0800

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Functional Programming Exercise/TriFunction/TriFunction/Program.cs
@@ -0,0 +1,11 @@
+﻿Func<string, int, bool> checkEqualOrLargerNameSum =
+    (name, sum) => name.Sum(ch => ch) >= sum;
+
+Func<string[], int, Func<string, int, bool>, string> getFirstName =
+    (names, sum, match) => names.FirstOrDefault(name => match(name, sum));
+
+int sum = int.Parse(Console.ReadLine());
+
+string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Console.WriteLine(getFirstName(names, sum, checkEqualOrLargerNameSum));
