bb90db65bcf2b7fcc67619dca086c144eb8df19e
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-01 12:07:34 -0800

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Defining Classes - Lab/Car Extension/Program.cs
@@ -0,0 +1,22 @@
+﻿
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car = new Car();
+
+            car.Make = "MK3";
+            car.Model = "VW";
+            car.Year = 1992;
+            car.FuelQuantity = 200;
+            car.FuelConsumption= 200;
+            car.Drive(200);
+
+            Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
