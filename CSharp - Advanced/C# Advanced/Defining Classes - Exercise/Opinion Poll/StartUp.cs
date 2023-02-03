83763a1375e19b13a5478879cc81908a3ca6edf8
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-03 08:35:00 -0800

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Defining Classes - Exercise/Opinion Poll/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using DefiningClasses;
+using System;
+using System.Collections.Immutable;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        List<Person> peopleOver30 = new();
+
+        int count = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < count; i++)
+        {
+            string[] personProperties = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Person person = new(personProperties[0], int.Parse(personProperties[1]));
+
+            if (person.Age > 30)
+            {
+                peopleOver30.Add(person);
+            }
+        }
+        foreach (var people in peopleOver30.OrderBy(p => p.Name))
+        {
+            Console.WriteLine($"{people.Name} - {people.Age}");
+        }
+    }
+}
