2219740dd7552ede3ed4be6a13f14b8ff410ed82
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-03 08:29:23 -0800

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Defining Classes - Exercise/Creating Constructors/DefiningClasses/DateModifier.cs
@@ -0,0 +1,15 @@
+﻿
+namespace DefiningClasses;
+
+public class DateModifier
+{
+    public static int GetDiffInDays(string start, string end)
+    {
+        DateTime startDate = DateTime.Parse(start);
+        DateTime endDate = DateTime.Parse(end);
+
+        TimeSpan difference = endDate - startDate;
+
+        return Math.Abs(difference.Days);
+    }
+}
---
+++ CSharp - Advanced/C# Advanced/Defining Classes - Exercise/Creating Constructors/DefiningClasses/Person.cs
@@ -0,0 +1,49 @@
+﻿namespace DefiningClasses;
+
+public class Person
+{
+    private string name;
+    private int age;
+
+    public Person()
+    {
+        Name = "No name";
+        Age = 1;
+    }
+
+    public Person(int age)
+        : this()
+    {
+        Age = age;
+    }
+
+    public Person(string name, int age)
+        : this(age)
+    {
+        Name = name;
+    }
+
+    public string Name
+    {
+        get
+        {
+            return this.name;
+        }
+        set
+        {
+            this.name = value;
+        }
+    }
+
+    public int Age
+    {
+        get
+        {
+            return this.age;
+        }
+        set
+        {
+            this.age = value;
+        }
+    }
+}
