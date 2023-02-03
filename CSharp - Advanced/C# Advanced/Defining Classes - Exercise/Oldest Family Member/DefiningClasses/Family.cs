a14707de0494401b858c0fbc69fc24c1a30d48f9
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-03 08:30:36 -0800

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Defining Classes - Exercise/Oldest Family Member/DefiningClasses/Family.cs
@@ -0,0 +1,34 @@
+﻿
+namespace DefiningClasses;
+
+public class Family
+{
+    private List<Person> people;
+
+    public Family()
+    {
+        this.people = new();
+    }
+
+    public List<Person> People
+    {
+        get
+        {
+            return this.people;
+        }
+        set
+        {
+            this.people = value;
+        }
+    }
+
+    public void AddMember(Person person)
+    {
+        this.people.Add(person);
+    }
+
+    public Person GetOldestMember()
+    {
+        return this.people.MaxBy(p => p.Age);
+    }
+}
