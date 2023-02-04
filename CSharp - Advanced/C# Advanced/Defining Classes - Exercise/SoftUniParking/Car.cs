116f17c54b401981d2f04676a7bc7a94995bbb21
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-04 08:00:11 -0800

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Defining Classes - Exercise/SoftUniParking/Car.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+
+namespace SoftUniParking;
+
+public class Car
+{
+    public Car(string make, string model, int horsePower, string registrationNumber)
+    {
+        Make = make;
+        Model = model;
+        HorsePower = horsePower;
+        RegistrationNumber = registrationNumber;
+    }
+
+    public string Make { get; set; }
+
+    public string Model { get; set; }
+
+    public int HorsePower { get; set; }
+
+    public string RegistrationNumber { get; set; }
+
+    public override string ToString()
+    {
+        return
+            $"Make: {Make}{Environment.NewLine}" +
+            $"Model: {Model}{Environment.NewLine}" +
+            $"HorsePower: {HorsePower}{Environment.NewLine}" +
+            $"RegistrationNumber: {RegistrationNumber}";
+
+    }
+}
