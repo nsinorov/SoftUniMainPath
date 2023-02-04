81400a9d22811ca114be2c68cf847483ac78842b
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-04 07:51:48 -0800

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Defining Classes - Exercise/Date Modifier/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using DefiningClasses;
+using System;
+using System.Collections.Immutable;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        string start = Console.ReadLine();
+        string end = Console.ReadLine();
+
+        int differenceInDays = DateModifier.GetDiffInDays(start, end);
+
+        Console.WriteLine(differenceInDays);
+    }
+}
