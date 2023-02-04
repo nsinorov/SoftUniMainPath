5f765bf7b96282aaa5ea342b466a91111fc17b55
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-04 07:54:13 -0800

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Defining Classes - Exercise/PokemonTrainer/PokemonTrainer/Program.cs
@@ -0,0 +1,42 @@
+﻿using PokemonTrainer;
+
+string command = Console.ReadLine();
+
+List<Trainer> trainers = new();
+
+
+while (command != "Tournament")
+{
+    string[] tokens = command.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+    Trainer trainer = trainers.SingleOrDefault(t => t.Name == tokens[0]);
+
+    if (trainer == null)
+    {
+        trainer = new(tokens[0]);
+        trainer.Pokemons.Add(new(tokens[1], tokens[2], int.Parse(tokens[3])));
+        trainers.Add(trainer);
+    }
+    else
+    {
+        trainer.Pokemons.Add(new(tokens[1], tokens[2], int.Parse(tokens[3])));
+    }
+    command = Console.ReadLine();
+}
+
+ command = Console.ReadLine();
+
+while (command != "End")
+{
+    foreach (var trainer in trainers)
+    {
+        trainer.CheckPokemon(command);
+    }
+
+    command = Console.ReadLine();
+}
+
+foreach (var trainer in trainers.OrderByDescending(t => t.NumberOfBadges))
+{
+    Console.WriteLine($"{trainer.Name} {trainer.NumberOfBadges} {trainer.Pokemons.Count}");
+}
---
+++ CSharp - Advanced/C# Advanced/Defining Classes - Exercise/PokemonTrainer/PokemonTrainer/Trainer.cs
@@ -0,0 +1,37 @@
+﻿
+namespace PokemonTrainer;
+
+public class Trainer
+{
+    public Trainer(string name)
+    {
+        Name = name;
+        Pokemons = new();
+    }
+
+    public string Name { get; set; }
+    public int NumberOfBadges { get; set; }
+    public List<Pokemon> Pokemons { get; set; }
+
+    public void CheckPokemon(string element)
+    {
+        if (Pokemons.Any(p => p.Element == element))
+        {
+            NumberOfBadges++;
+        }
+        else
+        {
+            for (int i = 0; i < Pokemons.Count; i++)
+            {
+                Pokemon currentPokemon = Pokemons[i];
+
+                currentPokemon.Health -= 10;
+
+                if (currentPokemon.Health <= 0)
+                {
+                    Pokemons.Remove(currentPokemon);
+                }
+            }
+        }
+    }
+}
