c9169e04096576f32892c42ae85040a1d92a0c0b
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-05 08:11:19 -0800

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Defining Classes - Exercise/SpeedRacing/SpeedRacing/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using SpeedRacing;
+public class Startup
+{
+    static void Main(string[] args)
+    {
+        int numOfCars = int.Parse(Console.ReadLine());
+
+        Dictionary<string, Car> carsByNames = new();
+
+        for (int i = 0; i < numOfCars; i++)
+        {
+
+            string[] carInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Car car = new()
+            {
+                Model = carInfo[0],
+                FuelAmount = double.Parse(carInfo[1]),
+                FuelConsumptionPerKilometer = double.Parse(carInfo[2])
+            };
+
+            carsByNames.Add(car.Model, car);
+        }
+        string command = Console.ReadLine();
+        while (command != "End")
+        {
+            string[] tokens = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string carModel = tokens[1];
+            double amountOfKilometers = double.Parse(tokens[2]);
+
+            Car car = carsByNames[carModel];
+
+            car.Drive(amountOfKilometers);
+
+            command = Console.ReadLine();
+        }
+
+        foreach (var car in carsByNames.Values)
+        {
+            Console.WriteLine($"{car.Model} {car.FuelAmount:f2} {car.TravelledDistance}");
+        }
+    }
+}
