b1699b450b96ad32699bc3b801a065246bff284c
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-06 13:28:02 -0800

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Implementing Stack and Queue/CustomList/Program.cs
@@ -0,0 +1,25 @@
+﻿using CustomListFromScratch;
+
+CustomList list = new();
+
+list.Add(100);
+list.Add(101);
+list.Add(102);
+list.Add(103);
+list.Add(104);
+
+list[1] = 324;
+
+list.Add(105);
+list.Add(106);
+list.Add(107);
+list.Add(108);
+
+Console.WriteLine(list[1]);
+
+Console.WriteLine(list.RemoveAt(0));
+Console.WriteLine(list.RemoveAt(0));
+Console.WriteLine(list.RemoveAt(0));
+Console.WriteLine(list.RemoveAt(0));
+
+Console.WriteLine(list.Count);
