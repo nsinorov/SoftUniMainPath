4f2fa5b214b36bfb93832574bea36097fb65c762
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-06 03:15:04 -0800

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Implementing Linked List/CustomDoublyLinkedList/CustomDoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,140 @@
+﻿
+namespace CustomDoublyLinkedList
+{
+    public class DoublyLinkedList
+    {
+        private class ListNode
+        {
+            public int Value { get; set; }
+
+            public ListNode NextNode { get; set; }
+
+            public ListNode PrivousNode { get; set; }
+
+            public ListNode(int value)
+            {
+                Value = value;
+            }
+        }
+
+        private ListNode head;
+        private ListNode tail;
+
+        public int Count { get; private set; }
+
+        public void AddFirst(int element)
+        {
+            var newHead = new ListNode(element);
+
+            if (Count == 0)
+            {
+                head = newHead;
+                tail = newHead;
+            }
+            else
+            {
+                newHead.NextNode = head;
+                head.PrivousNode = newHead;
+                head = newHead;
+            }
+
+            Count++;
+        }
+
+        public void AddLast(int element)
+        {
+            var newTail = new ListNode(element);
+
+            if (Count == 0)
+            {
+                head = newTail;
+                tail = newTail;
+            }
+            else
+            {
+                newTail.PrivousNode = tail;
+                tail.NextNode = newTail;
+                tail = newTail;
+            }
+            Count++;
+        }
+
+        public int RemoveFirst()
+        {
+            Validate();
+
+            var firstElement = head.Value;
+            head = head.NextNode;
+
+            if (head != null)
+            {
+                head.PrivousNode = null;
+            }
+            else
+            {
+                tail = null;
+            }
+
+            Count--;
+
+            return firstElement;
+        }
+
+     
+
+        public int RemoveLast()
+        {
+            Validate();
+
+            var lastElement = tail.Value;
+            tail = tail.PrivousNode;
+
+            if (tail != null)
+            {
+                tail.NextNode = null;
+            }
+            else
+            {
+                head = null;
+            }
+
+            Count--;
+
+            return lastElement;
+        }
+
+        public void ForEach(Action<int> action)
+        {
+            var currentNode = head;
+
+            while (currentNode != null)
+            {
+                action(currentNode.Value);
+                currentNode = currentNode.NextNode;
+            }
+        }
+
+        public int[] ToArray()
+        {
+            int[] array = new int[Count];
+            int counter = 0;
+            var currentNode = head;
+
+            while (currentNode != null)
+            {
+                array[counter] = currentNode.Value;
+                currentNode = currentNode.NextNode;
+                counter++;
+            }
+
+            return array;
+        }
+        private void Validate()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+        }
+    }
+}
---
+++ CSharp - Advanced/C# Advanced/Implementing Linked List/CustomDoublyLinkedList/CustomDoublyLinkedList/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using CustomDoublyLinkedList;
+
+public class StartUp
+{
+    private static void Main(string[] args)
+    {
+        DoublyLinkedList nums = new DoublyLinkedList();
+
+        nums.AddFirst(1);
+        nums.AddFirst(2);
+        nums.AddFirst(3);
+        nums.AddFirst(4);
+
+        nums.AddLast(5);
+        nums.AddLast(6);
+        nums.AddLast(7);
+        nums.AddLast(8);
+        nums.AddLast(9);
+
+        nums.RemoveFirst();
+        nums.RemoveLast();
+
+        nums.ForEach(x => { });
+        nums.ToArray();
+
+        foreach (var letters in nums.ToArray())
+        {
+            Console.Write($"{letters} ");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine(nums.Count);
+    }
+}
