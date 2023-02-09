e6340e48c58c3697d3d264813e6c8266337b0854
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-09 10:31:13 -0800

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Generics - Lab/GenericScale/GenericScale/StartUp .cs
@@ -0,0 +1,12 @@
+﻿namespace GenericScale;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        EqualityScale<int> scale = new EqualityScale<int>(11, 111);
+        Console.WriteLine(scale.AreEqual());
+
+      // Console.WriteLine(scale.GetBigger());
+    }
+}
