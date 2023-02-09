761a5c3968cc5267577e9295937fee84d6b0cc56
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-09 10:19:02 -0800

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Generics - Lab/GenericArrayCreator/GenericArrayCreator/StartUp .cs
@@ -0,0 +1,13 @@
+﻿namespace GenericArrayCreator;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {   
+        string[] strings = ArrayCreator.Create(5, "Pesho");
+        Console.WriteLine(String.Join(" ", strings));
+
+        int[] integers = ArrayCreator.Create(10, 33);
+        Console.WriteLine(String.Join("", integers));
+    }
+}
