874f40dbded9e4154389bf793ca403ed120dcef2
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-09 10:34:58 -0800

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Generics - Exercise/Generic Box of Integer/Program.cs
@@ -0,0 +1,14 @@
+﻿using GenericBoxOfString;
+
+Box<int> box = new();
+
+int numOfInput = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numOfInput; i++)
+{
+    int input = int.Parse(Console.ReadLine());
+    box.Add(input);
+
+}
+
+Console.WriteLine(box.ToString());
