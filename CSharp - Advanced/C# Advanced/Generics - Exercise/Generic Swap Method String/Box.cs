966999eb90c8cd2eb6f53b3e3aa92cd060380f23
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-10 13:14:05 -0800

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Generics - Exercise/Generic Swap Method String/Box.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Text;
+
+namespace GenericBoxOfString
+{
+    public class Box<T>
+    {
+
+        private List<T> items;
+
+        public Box()
+        {
+            items = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public void Swap(int firstItem, int secondItem)
+        {
+            T temp = items[firstItem];
+            items[firstItem] = items[secondItem];
+            items[secondItem] = temp;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new();
+
+            foreach (T item in items)
+            {
+                stringBuilder.AppendLine($"{typeof(T)}: {item}");
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
