31ada186dee511380dc2982c52dba1ae51c7e56c
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-10 13:09:23 -0800

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Generics - Exercise/Generic Box of String/Program.cs
@@ -0,0 +1,14 @@
+﻿using GenericBoxOfString;
+
+Box<string> box = new();
+
+int numOfInput = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numOfInput; i++)
+{
+    string input = Console.ReadLine();
+    box.Add(input);
+
+}
+
+Console.WriteLine(box.ToString());
