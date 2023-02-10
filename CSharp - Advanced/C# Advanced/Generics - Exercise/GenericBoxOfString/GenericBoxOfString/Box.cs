5b4a283ef44bb06b1eee3fac9550b55b8590404a
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-10 13:18:15 -0800

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Generics - Exercise/GenericBoxOfString/GenericBoxOfString/Box.cs
@@ -0,0 +1,55 @@
+﻿
+using System.Text;
+
+namespace GenericBoxOfString
+{
+    public class Box<T> where T : IComparable<T>
+    {
+
+        private List<T> items;
+
+        public Box()
+        {
+            items = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public void Swap(int firstItem, int secondItem)
+        {
+            T temp = items[firstItem];
+            items[firstItem] = items[secondItem];
+            items[secondItem] = temp;
+        }
+
+        public int Count(T itemsToCompare)
+        {
+            int count = 0;
+
+            foreach (var item in items)
+            {
+                if (item.CompareTo(itemsToCompare) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new();
+
+            foreach (T item in items)
+            {
+                stringBuilder.AppendLine($"{typeof(T)}: {item}");
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
---
+++ CSharp - Advanced/C# Advanced/Generics - Exercise/GenericBoxOfString/GenericBoxOfString/Program.cs
@@ -0,0 +1,16 @@
+﻿using GenericBoxOfString;
+
+Box<double> box = new();
+
+double count = double.Parse(Console.ReadLine());
+
+for (int i = 0; i < count; i++)
+{
+    double item = double.Parse(Console.ReadLine());
+
+    box.Add(item);
+}
+
+double itemToCompare = double.Parse(Console.ReadLine());
+
+Console.WriteLine(box.Count(itemToCompare));
