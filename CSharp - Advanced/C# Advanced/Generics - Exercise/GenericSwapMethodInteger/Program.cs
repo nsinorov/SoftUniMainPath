6718d397d9d241af02c72fb4ac2e15e815fbb50d
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-11 13:15:47 -0800

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Generics - Exercise/GenericSwapMethodInteger/Program.cs
@@ -0,0 +1,21 @@
+﻿using GenericBoxOfString;
+
+Box<int> box = new();
+
+int count = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < count; i++)
+{
+    int item = int.Parse(Console.ReadLine());
+
+    box.Add(item);
+}
+
+int[] indices = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+box.Swap(indices[0], indices[1]);
+
+Console.WriteLine(box.ToString());
