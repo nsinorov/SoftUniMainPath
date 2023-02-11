667c4fcc6133e9ca142147b79c594a33ba265ee8
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-11 13:18:10 -0800

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Generics - Exercise/Generic Count Method String/Program.cs
@@ -0,0 +1,16 @@
+﻿using GenericBoxOfString;
+
+Box<string> box = new();
+
+int count = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < count; i++)
+{
+    string item = Console.ReadLine();
+
+    box.Add(item);
+}
+
+string itemToCompare = Console.ReadLine();
+
+Console.WriteLine(box.Count(itemToCompare));
