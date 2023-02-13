783ea044b9b3afa4e2ac7fd2b3b769a7201c7e03
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-13 20:18:50 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Sets and Dictionaries Advanced - Lab/Average Student Grades/Average Student Grades/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+int numOfStudents = int.Parse(Console.ReadLine());
+
+Dictionary<string, List<decimal>> students = new Dictionary<string, List<decimal>>();
+
+for (int i = 0; i < numOfStudents; i++)
+{
+    string[] input = Console.ReadLine().Split();
+    string studentName = input[0];
+    decimal grade = decimal.Parse(input[1]);
+
+    if (!students.ContainsKey(studentName))
+    {
+        students.Add(studentName, new List<decimal>());
+    }
+
+    students[studentName].Add(grade);
+
+}
+
+foreach (var studentGrade in students)
+{
+    Console.WriteLine($"{studentGrade.Key} -> {string.Join(" ", studentGrade.Value.Select(x => $"{x:F2}").ToList())} (avg: {studentGrade.Value.Average():F2})");
+}
