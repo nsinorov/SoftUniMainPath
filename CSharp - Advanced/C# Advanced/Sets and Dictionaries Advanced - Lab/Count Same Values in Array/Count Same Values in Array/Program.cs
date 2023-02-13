b9d08c1741a3022096629e2216f613b0b5945104
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-13 20:23:16 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Sets and Dictionaries Advanced - Lab/Count Same Values in Array/Count Same Values in Array/Program.cs
@@ -0,0 +1,18 @@
+﻿double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();   
+
+Dictionary<double, int> occurences = new Dictionary<double, int>();
+
+foreach (var number in numbers)
+{
+    if (!occurences.ContainsKey(number))
+    {
+        occurences.Add(number, 0);
+    }
+
+    occurences[number]++;
+}
+
+foreach (var item in occurences)
+{
+    Console.WriteLine($"{item.Key} - {item.Value} times");
+}
