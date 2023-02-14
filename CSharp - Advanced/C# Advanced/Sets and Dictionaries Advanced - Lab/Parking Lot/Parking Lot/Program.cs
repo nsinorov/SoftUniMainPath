62d6e20ec9bdff1d52a044ee6b95478233480318
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-14 19:10:44 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Sets and Dictionaries Advanced - Lab/Parking Lot/Parking Lot/Program.cs
@@ -0,0 +1,33 @@
+﻿var input = Console.ReadLine();
+
+HashSet<string> parking = new();
+
+while(input != "END")
+{
+    string[] tokens = input.Split(", ");
+    string command = tokens[0];
+    string carPlate = tokens[1];
+
+    if (command == "IN")
+    {
+        parking.Add(carPlate);
+    }
+    else
+    {
+        parking.Remove(carPlate);
+    }
+
+    input = Console.ReadLine();
+}
+
+if(parking.Count > 0)
+{
+    foreach (var car in parking)
+    {
+        Console.WriteLine(car);
+    }
+}
+else
+{
+    Console.WriteLine("Parking Lot is Empty");
+}
