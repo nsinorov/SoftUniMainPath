2e4f87de7b47bf10cc46e4bb00eb3207b72606ad
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-14 19:15:24 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Sets and Dictionaries Advanced - Lab/Record Unique Names/Record Unique Names/Program.cs
@@ -0,0 +1,13 @@
+﻿int numsOfInput = int.Parse(Console.ReadLine());
+
+HashSet<string> names = new HashSet<string>();
+
+for (int i = 0; i < numsOfInput; i++)
+{
+
+    names.Add(Console.ReadLine());
+}
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
