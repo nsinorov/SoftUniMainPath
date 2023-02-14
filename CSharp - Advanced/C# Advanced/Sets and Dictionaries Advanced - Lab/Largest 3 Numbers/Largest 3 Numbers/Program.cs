37d4d2c427a70d5035123f645811d37122f03131
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-14 19:08:13 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Sets and Dictionaries Advanced - Lab/Largest 3 Numbers/Largest 3 Numbers/Program.cs
@@ -0,0 +1,3 @@
+﻿List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+numbers = numbers.OrderByDescending(x => x).Take(3).ToList();Console.WriteLine(string.Join(" ", numbers));
