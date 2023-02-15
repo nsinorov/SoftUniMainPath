ee5e6302220ca8e6e0f1e7b1bfe732e580d18a94
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-15 18:25:33 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Sets and Dictionaries Advanced - Exercise/Count Symbols/Count Symbols/Program.cs
@@ -0,0 +1,18 @@
+﻿string input = Console.ReadLine();
+
+SortedDictionary<char, int> charsCount = new();
+
+foreach (var ch in input)
+{
+    if (!charsCount.ContainsKey(ch))
+    {
+        charsCount.Add(ch, 0);
+    }
+
+    charsCount[ch]++;
+}
+
+foreach (var charCount in charsCount)
+{
+    Console.WriteLine($"{charCount.Key}: {charCount.Value} time/s");
+}
