c3c520d9df595253e437656081b121ff7cfe8de5
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-15 18:27:42 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Sets and Dictionaries Advanced - Exercise/Even Times/Even Times/Program.cs
@@ -0,0 +1,17 @@
+﻿int numOfUnputs = int.Parse(Console.ReadLine());
+
+Dictionary<int, int> numOfOccurences= new();
+
+for (int i = 0; i < numOfUnputs; i++)
+{
+    int nums = int.Parse(Console.ReadLine());
+
+    if (!numOfOccurences.ContainsKey(nums))
+    {
+        numOfOccurences.Add(nums, 0);
+    }
+
+    numOfOccurences[nums]++;
+}
+
+Console.WriteLine(numOfOccurences.Single(n => n.Value % 2 == 0).Key);
