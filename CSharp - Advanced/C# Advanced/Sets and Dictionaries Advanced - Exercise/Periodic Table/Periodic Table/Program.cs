8e02b58299c0768f3e0eadc00cfe0fe9268d955c
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-16 12:34:59 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Sets and Dictionaries Advanced - Exercise/Periodic Table/Periodic Table/Program.cs
@@ -0,0 +1,11 @@
+﻿int numOfInputs = int.Parse(Console.ReadLine());
+
+SortedSet<string> inputs = new();
+
+for (int i = 0; i < numOfInputs; i++)
+{
+    string[] chemicalCompounds = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    inputs.UnionWith(chemicalCompounds);
+}
+
+Console.Write(string.Join(" ", inputs));
