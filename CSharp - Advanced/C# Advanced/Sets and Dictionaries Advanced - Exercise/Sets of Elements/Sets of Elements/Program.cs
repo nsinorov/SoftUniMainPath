4dc641c480ea201b966154785a1488b55b9f4cb2
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-16 12:39:37 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Sets and Dictionaries Advanced - Exercise/Sets of Elements/Sets of Elements/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+HashSet<int> firstNums = new();
+HashSet<int> secondNums = new();
+
+for (int i = 0; i < nums[0]; i++)
+{
+    firstNums.Add(int.Parse(Console.ReadLine()));
+
+}
+for (int i = 0; i < nums[1]; i++)
+{
+    secondNums.Add(int.Parse(Console.ReadLine()));
+}
+
+firstNums.IntersectWith(secondNums);
+
+Console.WriteLine(string.Join(" ", firstNums));
