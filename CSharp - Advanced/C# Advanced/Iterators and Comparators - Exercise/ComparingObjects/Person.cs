3ef99e6fa67597dcf8bacf39fb5201028684d7a5
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-17 23:14:52 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Iterators and Comparators - Exercise/ComparingObjects/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Town { get; set; }
+
+        public int CompareTo(Person other)
+        {
+           int result = Name.CompareTo(other.Name);
+
+            if (result != 0)
+            {
+                return result;
+            }
+
+            result = Age.CompareTo(other.Age);
+
+            if(result != 0)
+            {
+                return result;
+            }
+
+            return Town.CompareTo(other.Town);
+        }
+    }
+}
---
+++ CSharp - Advanced/C# Advanced/Iterators and Comparators - Exercise/ComparingObjects/Program.cs
@@ -0,0 +1,48 @@
+﻿using ComparingObjects;
+
+List<Person> people = new();
+
+string command = string.Empty;
+
+while ((command = Console.ReadLine()) != "END")
+{
+    string[] personInfo = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    Person person = new()
+    {
+        Name = personInfo[0],
+        Age = int.Parse(personInfo[1]),
+        Town = personInfo[2]
+    };
+
+    people.Add(person);
+}
+
+int compareIndex = int.Parse(Console.ReadLine()) - 1;
+
+Person personToCompare = people[compareIndex];
+
+int equalCount = 0;
+int diffCount = 0;
+
+foreach (Person person in people)
+{
+    if (person.CompareTo(personToCompare) == 0)
+    {
+        equalCount++;
+    }
+    else
+    {
+        diffCount++;
+    }
+}
+
+if (equalCount == 1)
+{
+    Console.WriteLine("No matches");
+}
+else
+{
+    Console.WriteLine($"{equalCount} {diffCount} {people.Count}");
+}
+
