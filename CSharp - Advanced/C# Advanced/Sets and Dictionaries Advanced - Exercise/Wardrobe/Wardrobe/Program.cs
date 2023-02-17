c6a4dce7abe5cf84bc707b88375ef1c66e34d400
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-17 22:57:02 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Sets and Dictionaries Advanced - Exercise/Wardrobe/Wardrobe/Program.cs
@@ -0,0 +1,46 @@
+﻿int numOfInputs = int.Parse(Console.ReadLine());
+
+Dictionary<string, Dictionary<string, int>> clothingColor = new();
+
+for (int i = 0; i < numOfInputs; i++)
+{
+    string[] tokens = Console.ReadLine().Split(new string[] { " -> ", "," }, StringSplitOptions.RemoveEmptyEntries);
+
+    string color = tokens[0];
+
+    if (!clothingColor.ContainsKey(color))
+    {
+        clothingColor[color] = new Dictionary<string, int>();
+    }
+
+    for (int j = 1; j < tokens.Length; j++)
+    {
+        string currentCloth = tokens[j];
+
+        if (!clothingColor[color].ContainsKey(currentCloth))
+        {
+            clothingColor[color].Add(currentCloth, 0);
+        }
+
+        clothingColor[color][currentCloth]++;
+    }
+}
+
+string[] desiredCloth = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+foreach (var clothByColor in clothingColor)
+{
+    Console.WriteLine($"{clothByColor.Key} clothes:");
+
+    foreach (var cloth in clothByColor.Value)
+    {
+        string printItem = $"* {cloth.Key} - {cloth.Value}";
+
+        if (clothByColor.Key == desiredCloth[0] && cloth.Key == desiredCloth[1])
+        {
+            printItem += " (found!)";
+        }
+
+        Console.WriteLine(printItem);
+    }
+}
