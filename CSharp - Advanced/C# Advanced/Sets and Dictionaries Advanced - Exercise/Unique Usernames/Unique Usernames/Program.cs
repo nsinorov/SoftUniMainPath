d93d5e6925058a6c78cdd6f96f0f861017cc9d3c
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-17 22:55:27 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Sets and Dictionaries Advanced - Exercise/Unique Usernames/Unique Usernames/Program.cs
@@ -0,0 +1,17 @@
+﻿int numOfInput = int.Parse(Console.ReadLine());
+
+HashSet<string> names = new();
+
+for (int i = 0; i < numOfInput; i++)
+{
+
+    string name = Console.ReadLine();
+    names.Add(name);
+
+
+}
+
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
