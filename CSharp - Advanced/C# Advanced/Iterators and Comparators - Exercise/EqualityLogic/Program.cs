e9c893f7d4d95c665c88333126a86149f5b39023
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-18 23:02:25 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Iterators and Comparators - Exercise/EqualityLogic/Program.cs
@@ -0,0 +1,25 @@
+﻿using EqualityLogic;
+
+HashSet<Person> hashSet = new();
+
+SortedSet<Person> sortedSet = new();
+
+int count = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < count; i++)
+{
+    string[] personInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    Person persom = new()
+    {
+        Name = personInfo[0],
+        Age = int.Parse(personInfo[1]),
+    };
+
+    hashSet.Add(persom);
+    sortedSet.Add(persom);
+
+}
+
+Console.WriteLine(hashSet.Count);
+Console.WriteLine(sortedSet.Count);
