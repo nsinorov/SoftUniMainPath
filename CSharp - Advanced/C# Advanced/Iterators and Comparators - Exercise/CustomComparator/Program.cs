f9f593b6ebccd181aa02c4274f1bb752867e9c07
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-18 22:58:56 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Iterators and Comparators - Exercise/CustomComparator/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+Func<int, int, int> customComparator = (x, y) =>
+{
+    if (x % 2 == 0 && y % 2 != 0)
+    {
+        return -1;
+    }
+    if (x % 2 != 0 && y % 2 == 0)
+    {
+        return 1;
+    }
+
+    return x.CompareTo(y);
+
+};
+
+int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+Array.Sort(numbers, (x, y) => customComparator(x, y));
+
+Console.WriteLine(string.Join(" ", numbers));
