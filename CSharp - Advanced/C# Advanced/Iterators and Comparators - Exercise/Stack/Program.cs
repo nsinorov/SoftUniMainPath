1979ad2c5baa1309daf4c0fda0c66e65d5a92fe6
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-19 21:53:04 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Iterators and Comparators - Exercise/Stack/Program.cs
@@ -0,0 +1,41 @@
+﻿using Stack;
+
+CustomStack<string> customStack = new();
+
+string command = string.Empty;
+
+while((command = Console.ReadLine()) != "END")
+{
+    string[] tokens = command.Split(new char[] { ' ', ',' },StringSplitOptions.RemoveEmptyEntries);
+
+    string action = tokens[0];
+
+    if(action == "Push")
+    {
+        foreach (var item in tokens.Skip(1))
+        {
+            customStack.Push(item);
+        }
+    }
+    else
+    {
+        try
+        {
+            customStack.Pop();
+        }
+        catch (Exception exception)
+        {
+
+            Console.WriteLine(exception.Message);
+        }
+    }
+}
+
+foreach (var item in customStack)
+{
+    Console.WriteLine(item);
+}
+foreach (var item in customStack)
+{
+    Console.WriteLine(item);
+}
