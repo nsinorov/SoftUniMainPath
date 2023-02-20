2c3bb200adc91cebe217959651c2936169ea5f0e
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-20 20:54:45 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# Advanced/Exam Preparation/ComputerArchitecture/ComputerArchitecture/Computer.cs
@@ -0,0 +1,80 @@
+﻿
+
+using System.Text;
+
+namespace ComputerArchitecture
+{
+    public class Computer
+    {
+        public Computer(string model, int capacity)
+        {
+            Capacity = capacity;
+            Model = model;
+            Multiprocessor = new List<CPU>();
+        }
+        public List<CPU> Multiprocessor { get; set; }
+
+        public int Capacity { get; set; }
+
+        public string Model { get; set; }
+
+        public int Count { get { return Multiprocessor.Count; } }
+
+        public void Add(CPU cpu)
+        {
+            if (Multiprocessor.Count < Capacity)
+            {
+                Multiprocessor.Add(cpu);
+            }
+        }
+
+        public bool Remove(string brand)
+        {
+            CPU cpuToRemove = Multiprocessor.FirstOrDefault(c => c.Brand == brand);
+            if (cpuToRemove != null)
+            {
+                return Multiprocessor.Remove(cpuToRemove);
+            }
+
+            return false;
+        }
+
+        public CPU MostPowerful()
+        {
+            if (Multiprocessor.Count == 0)
+            {
+                return null;
+            }
+
+            CPU mostPowerful = Multiprocessor[0];
+
+            foreach (var cpu in Multiprocessor)
+            {
+                if (cpu.Frequency > mostPowerful.Frequency)
+                {
+                    mostPowerful = cpu;
+                }
+            }
+
+            return mostPowerful;
+        }
+
+        public CPU GetCPU(string brand)
+        {
+            return Multiprocessor.FirstOrDefault(c => c.Brand == brand);
+        }
+
+        public string Report()
+        {
+            StringBuilder result = new StringBuilder();
+
+            result.AppendLine($"CPUs in the Computer {Model}:");
+            foreach (var cpu in Multiprocessor)
+            {
+                result.AppendLine(cpu.ToString());
+            }
+
+            return result.ToString().Trim();
+        }
+    }
+}
