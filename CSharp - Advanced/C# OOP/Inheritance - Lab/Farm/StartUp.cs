121891a29da80790682f27c4ec986a11a3e6fd65
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-23 02:52:13 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# OOP/Inheritance - Lab/Farm/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Bark();
+            dog.Bark();
+            dog.Eat();
+        }
+    }
+}
