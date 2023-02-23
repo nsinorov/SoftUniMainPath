96e62ef0a3ff98341ee8869e8f23c13217569aed
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-23 03:33:13 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# OOP/Inheritance - Lab/CustomStack/CustomStack/StackOfStrings.cs
@@ -0,0 +1,19 @@
+﻿
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return Count == 0;
+        }
+
+        public void AddRange(IEnumerable<string> collection)
+        {
+            foreach (var item in collection)
+            {
+                this.Push(item);
+            }
+        }
+    }
+}
---
+++ CSharp - Advanced/C# OOP/Inheritance - Lab/CustomStack/CustomStack/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stack = new StackOfStrings();
+
+            stack.AddRange(args);
+            stack.IsEmpty();
+        }
+    }
+}
