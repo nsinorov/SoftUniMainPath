101638513d57688b3a2a60699a8cbfd552f2cafc
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-23 02:56:59 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# OOP/Inheritance - Lab/Multiple Inheritance/Puppy.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Farm
+{
+    public class Puppy : Dog
+    {
+        public void Weep()
+        {
+            Console.WriteLine("weeping...");
+        }
+    }
+}
---
+++ CSharp - Advanced/C# OOP/Inheritance - Lab/Multiple Inheritance/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Bark();
+            dog.Bark();
+            dog.Eat();
+
+            Puppy puppy = new Puppy();
+
+            puppy.Bark();
+            puppy.Eat();
+            puppy.Weep();
+        }
+    }
+}
