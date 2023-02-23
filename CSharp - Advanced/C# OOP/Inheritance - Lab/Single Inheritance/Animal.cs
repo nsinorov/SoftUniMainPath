f8586fc08f07f73f69b6508c3a6c6dee662c1133
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-23 02:53:28 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# OOP/Inheritance - Lab/Single Inheritance/Animal.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Farm
+{
+    public class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("eating...");
+        }
+    }
+}
---
+++ CSharp - Advanced/C# OOP/Inheritance - Lab/Single Inheritance/Dog.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Farm
+{
+    public class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("barking...");
+        }
+    }
+}
