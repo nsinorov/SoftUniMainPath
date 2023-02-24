b0ffddd6c75369978eb1752c4cbebb2f5a087e5e
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-24 13:52:17 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# OOP/Inheritance - Exercise/Person/Child.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Person
+{
+    public class Child : Person
+    {
+        public Child(string name, int age) : base(name, age)
+        {
+        }
+    }
+}
---
+++ CSharp - Advanced/C# OOP/Inheritance - Exercise/Person/Person.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Person
+{
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString() => $"Name: {Name}, Age: {Age}";
+    }
+}
