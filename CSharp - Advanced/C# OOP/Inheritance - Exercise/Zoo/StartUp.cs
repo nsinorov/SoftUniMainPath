4530ef066fef140398d2e87f87e7646132dd15ac
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-24 23:28:23 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# OOP/Inheritance - Exercise/Zoo/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Zoo
+{
+    public abstract class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Lizard lizard = new("Gosho");
+
+            Console.WriteLine(lizard.Name);
+        }
+    }
+}
