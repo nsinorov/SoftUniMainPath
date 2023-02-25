70bc8d85a275788a052b2498632cf17cb0a31d91
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-25 20:30:01 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# OOP/Inheritance - Exercise/PlayersAndMonsters/Hero.cs
@@ -0,0 +1,21 @@
+﻿
+namespace PlayersAndMonsters
+{
+    public abstract class Hero
+    {
+        protected Hero(string username, int level)
+        {
+            Username = username;
+            Level = level;
+        }
+
+        public string Username { get; set; }
+
+        public int Level { get; set; }
+
+        public override string ToString()
+        {
+            return $"Type: {this.GetType().Name} Username: {this.Username} Level: {this.Level}";
+        }
+    }
+}
---
+++ CSharp - Advanced/C# OOP/Inheritance - Exercise/PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            SoulMaster soulMaster = new("Dark", 100);
+
+            Console.WriteLine(soulMaster.Username);
+            Console.WriteLine(soulMaster.Level);
+        }
+    }
+}
