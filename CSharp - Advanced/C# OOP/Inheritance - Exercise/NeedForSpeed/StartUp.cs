85157cf45929188d60fedb10d64fec89b9fb5c7b
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-26 18:15:43 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# OOP/Inheritance - Exercise/NeedForSpeed/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            SportCar sportCar = new(200, 20);
+
+            sportCar.Drive(5);
+
+            Console.WriteLine(sportCar.Fuel);
+            Console.WriteLine(sportCar.HorsePower);
+            Console.WriteLine(sportCar.FuelConsumption);
+
+            Motorcycle motor = new(150, 40);
+
+            motor.Drive(5);
+            Console.WriteLine(motor.Fuel);
+            Console.WriteLine(motor.HorsePower);
+            Console.WriteLine(motor.FuelConsumption);
+        }
+    }
+}
