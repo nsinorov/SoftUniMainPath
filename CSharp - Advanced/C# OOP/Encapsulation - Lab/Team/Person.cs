711fd1b45b86a2da45f6f94b0c84f7fe75caf320
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-27 21:33:21 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# OOP/Encapsulation - Lab/Team/Person.cs
@@ -0,0 +1,101 @@
+﻿
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;          
+        }
+
+        private string firstName;
+
+        public string FirstName 
+        {
+            get
+            {
+                return firstName;
+            }
+            private set
+            {
+                if(value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+
+                firstName= value;
+            }
+        }
+
+        private string lastName;
+		public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+                lastName = value;
+            }
+        }
+
+        private int age;
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+                age = value;
+            }
+        }
+        private decimal salary;
+        public decimal Salary
+        {
+            get
+            {
+                return salary;
+            }
+            private set
+            {
+                if (value < 650)
+                {
+                    throw new ArgumentException("Salary cannot be less than 650 leva!");
+                }
+                salary = value;
+            }
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if(Age > 30)
+            {
+                Salary += Salary * percentage / 100;
+            }
+            else
+            {
+                Salary += Salary * percentage / 200;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {Salary:F2} leva.";
+        }
+            
+    }
+}
