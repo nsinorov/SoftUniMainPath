e757b1316e6685b4cf69eb69ecf11fe2845f9c33
Nikolay Sinorov <nsinorov123@gmail.com>
2023-03-01 23:50:53 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# OOP/Encapsulation - Exercise/ClassBoxData/ClassBoxData/Box.cs
@@ -0,0 +1,62 @@
+﻿
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double Length
+        {
+            get => length;
+            set 
+            {
+               if(value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+
+               length = value;
+            }
+        }
+
+        public double Width
+        {
+            get => width;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+
+                width = value;
+            }
+        }
+
+        public double Height
+        {
+            get => height;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+
+                height = value;
+            }
+        }
+        public double SurfaceArea() => 2 * Length * Width + LateralSurfaceArea();
+        public double LateralSurfaceArea() => 2 * Length * Height + 2 * Width * Height;
+        public double Volume() => Length * Width * Height;
+    }
+}
---
+++ CSharp - Advanced/C# OOP/Encapsulation - Exercise/ClassBoxData/ClassBoxData/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using ClassBoxData;
+
+double lenght = double.Parse(Console.ReadLine());
+double width = double.Parse(Console.ReadLine());
+double height = double.Parse(Console.ReadLine());
+
+try
+{
+    Box box = new(lenght, width, height);
+
+    Console.WriteLine($"Surface Area - {box.SurfaceArea():f2}");
+    Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():f2}");
+    Console.WriteLine($"Volume - {box.Volume():f2}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
