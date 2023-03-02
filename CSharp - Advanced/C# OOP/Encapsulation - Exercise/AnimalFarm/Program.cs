da57b1dcd10eba1e56e20e9ac8e592d38c9601d2
Nikolay Sinorov <nsinorov123@gmail.com>
2023-03-02 12:18:35 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# OOP/Encapsulation - Exercise/AnimalFarm/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace AnimalFarm
+{
+    using System;
+    using AnimalFarm.Models;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Chicken chicken = new(name, age);
+                Console.WriteLine(chicken.ToString());
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
