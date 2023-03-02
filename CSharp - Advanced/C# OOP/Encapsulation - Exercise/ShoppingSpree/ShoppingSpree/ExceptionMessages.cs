edc2aad9e09a66b7337327a26f64e9a5f7b78a37
Nikolay Sinorov <nsinorov123@gmail.com>
2023-03-02 21:26:10 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# OOP/Encapsulation - Exercise/ShoppingSpree/ShoppingSpree/ExceptionMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace ShoppingSpree
+{
+    public class ExceptionMessages
+    {
+        public const string NameExceptionMessage = "Name cannot be empty";
+        public const string MoneyExeptionMessage = "Money cannot be negative";
+    }
+}
