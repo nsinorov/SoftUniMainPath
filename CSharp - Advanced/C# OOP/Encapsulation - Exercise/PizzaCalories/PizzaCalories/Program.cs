031c28dd87276103271f373a98a55f6eb6e614db
Nikolay Sinorov <nsinorov123@gmail.com>
2023-03-02 21:28:37 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# OOP/Encapsulation - Exercise/PizzaCalories/PizzaCalories/Program.cs
@@ -0,0 +1,35 @@
+﻿using PizzaCalories.Models;
+
+try
+{
+    string[] pizzaTokens = Console.ReadLine().Split();
+    string[] doughTokens = Console.ReadLine().Split();
+
+    string pizzaName = pizzaTokens[1];
+
+    Dough dough = new(doughTokens[1], doughTokens[2], double.Parse(doughTokens[3]));
+
+    Pizza pizza = new(pizzaName, dough);
+
+    while (true)
+    {
+        string toppingsInput = Console.ReadLine();
+
+        if (toppingsInput == "END")
+        {
+            break;
+        }
+
+        string[] toppingsTokens = toppingsInput.Split();
+
+        Topping topping = new(toppingsTokens[1], double.Parse(toppingsTokens[2]));
+
+        pizza.AddTopping(topping);
+    }
+
+    Console.WriteLine(pizza);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
