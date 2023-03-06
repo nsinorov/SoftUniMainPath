07e9a06f487b614f4a333c24281a2356537c8f48
Nikolay Sinorov <nsinorov123@gmail.com>
2023-03-06 23:05:38 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# OOP/Polymorphism - Lab/Animals/Animals/Animal.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Animals;
+
+public abstract class Animal
+{
+    protected Animal(string name, string favouriteFood)
+    {
+        Name = name;
+        FavouriteFood = favouriteFood;
+    }
+
+    public string Name { get; set; }
+    public string FavouriteFood { get; set; }
+    public virtual string ExplainSelf()
+    {
+        return "";
+    }
+}
---
+++ CSharp - Advanced/C# OOP/Polymorphism - Lab/Animals/Animals/Cat.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Animals;
+
+public class Cat : Animal
+{
+    public Cat(string name, string favouriteFood) : base(name, favouriteFood)
+    {
+    }
+
+    public override string ExplainSelf() => $"I am {Name} and my fovourite food is {FavouriteFood} MEEOW";   
+}
