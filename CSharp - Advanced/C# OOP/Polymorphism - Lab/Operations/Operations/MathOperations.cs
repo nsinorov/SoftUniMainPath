c3722db0b5fcbee5468ba04820d1acd60829c148
Nikolay Sinorov <nsinorov123@gmail.com>
2023-03-06 23:03:40 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# OOP/Polymorphism - Lab/Operations/Operations/MathOperations.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Operations
+{
+    public class MathOperations
+    {
+        public int Add(int a, int b) => a + b;
+        public double Add(double a, double b, double c) => a + b + c;
+        public decimal Add(decimal a, decimal b, decimal c) => a + b + c;
+    }
+}
