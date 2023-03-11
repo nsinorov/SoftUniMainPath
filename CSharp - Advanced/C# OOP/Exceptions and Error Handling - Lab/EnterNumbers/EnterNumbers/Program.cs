3ab24a333c5cab64916cab41134423577295acc1
Nikolay Sinorov <nsinorov123@gmail.com>
2023-03-11 23:09:15 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# OOP/Exceptions and Error Handling - Lab/EnterNumbers/EnterNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] numbers = new int[10];
+int start = 1;
+int end = 100;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    try
+    {
+        numbers[i] = ReadNumber(start, end);
+        start = numbers[i];
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Invalid Number!");
+        i--;
+    }
+    catch (ArgumentOutOfRangeException e)
+    {
+        Console.WriteLine(e.Message);
+        i--;
+    }
+}
+
+Console.WriteLine(string.Join(", ", numbers));
+    
+    static int ReadNumber(int start, int end)
+{
+    int number = int.Parse(Console.ReadLine());
+
+    if (number <= start || number >= end)
+    {
+        throw new ArgumentOutOfRangeException("", $"Your number is not in range {start} - {end}!");
+    }
+    return number;
+}
