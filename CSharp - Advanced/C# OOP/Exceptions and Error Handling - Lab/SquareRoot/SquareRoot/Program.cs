6b06fb9384ca65cd4740eb37d5a0834abf7412e3
Nikolay Sinorov <nsinorov123@gmail.com>
2023-03-13 23:04:21 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# OOP/Exceptions and Error Handling - Lab/SquareRoot/SquareRoot/Program.cs
@@ -0,0 +1,23 @@
+﻿
+try
+{
+    int number = int.Parse(Console.ReadLine());
+
+    if (number < 0)
+    {
+        Console.WriteLine("Invalid number.");
+    }
+    else
+    {
+        double squareRoot = Math.Sqrt(number);
+        Console.WriteLine(squareRoot);
+    }
+}
+catch (FormatException)
+{
+    Console.WriteLine("Invalid input.");
+}
+finally
+{
+    Console.WriteLine("Goodbye.");
+}
