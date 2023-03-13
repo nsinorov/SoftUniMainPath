5b6bc6686f964270891476a58f154a5311cdbf29
Nikolay Sinorov <nsinorov123@gmail.com>
2023-03-13 23:19:07 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# OOP/Exceptions and Error Handling - Lab/SumOfIntegers/SumOfIntegers/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] elements = Console.ReadLine().Split(' ');
+
+int sum = 0;
+
+foreach (string element in elements)
+{
+    try
+    {
+        int num = Convert.ToInt32(element);
+        sum += num;
+        Console.WriteLine($"Element '{element}' processed - current sum: {sum}");
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine($"The element '{element}' is in wrong format!");
+        Console.WriteLine($"Element '{element}' processed - current sum: {sum}");
+    }
+    catch (OverflowException)
+    {
+        Console.WriteLine($"The element '{element}' is out of range!");
+        Console.WriteLine($"Element '{element}' processed - current sum: {sum}");
+    }
+}
+
+Console.WriteLine($"The total sum of all integers is: {sum}");
