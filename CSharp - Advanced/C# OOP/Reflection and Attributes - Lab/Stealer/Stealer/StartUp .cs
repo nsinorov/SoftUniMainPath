84f29c49bc9a2a6dab4aab91dffeab4658ddb2a7
Nikolay Sinorov <nsinorov123@gmail.com>
2023-03-17 16:25:27 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# OOP/Reflection and Attributes - Lab/Stealer/Stealer/StartUp .cs
@@ -0,0 +1,22 @@
+﻿
+
+
+
+
+using Stealer;
+using System.Security.Claims;
+
+namespace Stealer;
+
+ class StartUp
+{
+    public static void Main(string[] args)
+    {
+        var spy = new Spy();
+        var result = spy.StealFieldInfo("Hacker", "username", "password");
+
+        Console.WriteLine(result);
+        //password = mySuperSecretPassw0rd
+
+    }
+}
