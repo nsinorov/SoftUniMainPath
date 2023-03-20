d51c05bfb7b671eb414864e9c16a01dc94fa3e76
Nikolay Sinorov <nsinorov123@gmail.com>
2023-03-20 12:08:42 +0200

    Add files via upload

---
+++ CSharp - Advanced/C# OOP/Reflection and Attributes - Lab/HighQualityMistakes/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace Stealer;
+
+class StartUp
+{
+    public static void Main(string[] args)
+    {
+        var spy = new Spy();
+        var result = spy.AnalyzeAcessModifiers("Hacker");
+
+        Console.WriteLine(result);
+    }
+}
