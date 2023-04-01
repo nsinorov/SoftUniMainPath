9e71647bb9aa85775364a703f2ff258ec96063cd
Nikolay Sinorov <nsinorov123@gmail.com>
2023-04-01 23:17:13 +0300

    Add files via upload

---
+++ CSharp - Advanced/C# OOP/Design Patterns - Exercise/Prototype/Prototype/Models/Sandwich.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Prototype.Models;
+
+public class Sandwich : SandwichPrototype
+{
+    private string bread;
+    private string cheese;
+    private string meat;
+    private string veggies;
+
+    public Sandwich(string bread, string cheese, string meat, string veggies)
+    {
+        this.bread = bread;
+        this.cheese = cheese;
+        this.meat = meat;
+        this.veggies = veggies;
+    }
+
+    public override SandwichPrototype Clone()
+    {
+        Console.WriteLine($"Cloning sandwich with ingredients: {GetIngredients()}");
+
+        return MemberwiseClone() as SandwichPrototype;
+    }
+
+    private string GetIngredients() => $"{bread}, {meat}, {cheese}, {veggies}";
+}
---
+++ CSharp - Advanced/C# OOP/Design Patterns - Exercise/Prototype/Prototype/Program.cs
@@ -0,0 +1,15 @@
+﻿using Prototype.Models;
+
+SandwichMenu sandwichMenu = new();
+
+sandwichMenu["BLT"] = new Sandwich("Wheat", "Bacon", "", "Letuce, Tomato");
+sandwichMenu["PB&J"] = new Sandwich("White", "", "", "Peanut Butter, Jelly");
+sandwichMenu["Turkey"] = new Sandwich("Rye", "Turkey", "Swiss", "Letuce, Onion, Tomato");
+
+sandwichMenu["LoadedBLT"] = new Sandwich("Wheat", "Turkey, Bacon", "American", "Letuce, Tomato, Onion, Olives");
+sandwichMenu["ThreeMeatCombo"] = new Sandwich("Rye", "Turkey, Ham, Salami", "Provolone", "Letuce, Onion");
+sandwichMenu["Vegeterian"] = new Sandwich("Wheat", "", "", "Letuce, Onion, Tomato, Olives, Spinach");
+
+Sandwich sandwich1 = sandwichMenu["BLT"].Clone() as Sandwich;
+Sandwich sandwich2 = sandwichMenu["ThreeMeatCombo"].Clone() as Sandwich;
+Sandwich sandwich3 = sandwichMenu["Vegeterian"].Clone() as Sandwich;
