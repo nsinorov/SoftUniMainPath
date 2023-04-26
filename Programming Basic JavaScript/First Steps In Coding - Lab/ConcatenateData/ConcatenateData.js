2e87d1679fbeb700938f232cc75b9033fe3b30ce
Nikolay Sinorov <nsinorov123@gmail.com>
2023-04-26 22:21:51 +0300

    Add files via upload

---
+++ Programming Basic JavaScript/First Steps In Coding - Lab/ConcatenateData/ConcatenateData.js
@@ -0,0 +1,12 @@
+function concatenateDate(input)
+{
+let firstName = input[0];
+let secondName = input[1];
+let age = input[2];
+let town = input[3];
+
+console.log(`You are ${firstName} ${secondName}, a ${age}-years old person from ${town}.`);
+
+}
+
+concatenateDate(["Maria", "Ivanova", 20, "Sofia"]);
