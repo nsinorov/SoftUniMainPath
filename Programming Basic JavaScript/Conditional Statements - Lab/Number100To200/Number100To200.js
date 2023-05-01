e867bbc0bec62bc2da543d82798609901247dd1c
Nikolay Sinorov <nsinorov123@gmail.com>
2023-05-01 23:40:29 +0300

    Add files via upload

---
+++ Programming Basic JavaScript/Conditional Statements - Lab/Number100To200/Number100To200.js
@@ -0,0 +1,19 @@
+function number100To200(input)
+{
+    let num = Number(input[0]);
+
+    if(num < 100)
+    {
+        console.log("Less than 100");
+    }
+    else if(num >= 100 && num <= 200)
+    {
+        console.log("Between 100 and 200");
+    }
+    else if(num > 200)
+    {
+        console.log("Greater than 200");
+    }
+}
+
+number100To200(["95"]);
