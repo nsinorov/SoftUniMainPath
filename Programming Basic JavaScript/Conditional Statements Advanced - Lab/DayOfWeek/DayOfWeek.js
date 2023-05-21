f0ab9e6003b2c576a3eb9b9ddab08efaf2f0ce63
Nikolay Sinorov <nsinorov123@gmail.com>
2023-05-21 22:07:02 +0300

    Add files via upload

---
+++ Programming Basic JavaScript/Conditional Statements Advanced - Lab/DayOfWeek/DayOfWeek.js
@@ -0,0 +1,42 @@
+function dayOfWeek(input){
+
+    let num = Number(input.shift());
+
+    switch(num)
+    {
+        case 1:
+            console.log("Monday");
+            break;
+            case 2:
+                console.log("Tuesday");
+                break;
+                case 3:
+                    console.log("Wednesday");
+                    break;
+                    case 4:
+                        console.log("Thursday");
+                        break;
+                        case 5:
+                            console.log("Friday");
+                            break;
+                            case 6:
+                                console.log("Saturday");
+                                break;
+                                case 7:
+                                    console.log("Sunday");
+                                    break;
+                                    default: 
+                                    console.log("Error");
+                                    break;
+        
+    }
+}
+
+dayOfWeek(["1"]);
+dayOfWeek(["2"]);
+dayOfWeek(["3"]);
+dayOfWeek(["4"]);
+dayOfWeek(["5"]);
+dayOfWeek(["6"]);
+dayOfWeek(["7"]);
+dayOfWeek(["-1"]);
