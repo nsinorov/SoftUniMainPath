25ce5336610e3ac5174df1198c65706ccf00e1c0
Nikolay Sinorov <nsinorov123@gmail.com>
2023-05-21 22:07:22 +0300

    Add files via upload

---
+++ Programming Basic JavaScript/Conditional Statements Advanced - Lab/FruitOrVegetable/FruitOrVegetable.js
@@ -0,0 +1,30 @@
+function fruitOrVegetable(input){
+
+    let product = input.shift();
+
+    switch (product)
+            {
+                case "banana":
+                case "apple":
+                case "kiwi":
+                case "cherry":
+                case "lemon":
+                case "grapes":
+                    console.log("fruit");
+                    break;
+                case "tomato":
+                case "cucumber":
+                case "pepper":
+                case "carrot":
+                    console.log("vegetable");
+                    break;                
+                default:
+                    console.log("unknown");
+                    break;                            
+            }                         
+}
+
+fruitOrVegetable(["banana"])
+fruitOrVegetable(["apple"])
+fruitOrVegetable(["tomato"])
+fruitOrVegetable(["water"])
