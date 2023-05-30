539b4aeacf672b25b188c2c0f6c0b2bb5083c8be
Nikolay Sinorov <nsinorov123@gmail.com>
2023-05-30 23:55:51 +0300

    Add files via upload

---
+++ Programming Basic JavaScript/For Loop - Lab/Nums1ToNWithStep3/Nums1ToNWithStep3.js
@@ -0,0 +1,12 @@
+function nums1Step3(input){
+
+    let num = Number(input[0]);
+
+    for(let i = 1; i <= num; i+=3){
+        console.log(i);
+    }
+}
+
+nums1Step3(["10"])
+nums1Step3(["7"])
+nums1Step3(["15"])
