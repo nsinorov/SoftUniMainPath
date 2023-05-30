658eb3dbc1cdb043e2eec5aef5ce39beb40436c1
Nikolay Sinorov <nsinorov123@gmail.com>
2023-05-30 23:49:24 +0300

    Add files via upload

---
+++ Programming Basic JavaScript/For Loop - Lab/Character Sequence/Character Sequence.js
@@ -0,0 +1,13 @@
+function charSequence(input){
+
+    let text = input[0];
+
+    for (let index = 0; index < text.length; index++) {
+       
+        console.log(text[index]);
+        
+    }
+}
+
+charSequence(["softuni"])
+charSequence(["ice cream"])
