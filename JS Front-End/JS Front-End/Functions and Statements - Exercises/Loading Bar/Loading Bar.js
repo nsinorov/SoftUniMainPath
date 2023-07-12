6968c08e6bbf2a0a2ec7f74f351fe1fe410ecd35
Nikolay Sinorov <nsinorov123@gmail.com>
2023-07-12 23:21:29 +0300

    Add files via upload

---
+++ JS Front-End/JS Front-End/Functions and Statements - Exercises/Loading Bar/Loading Bar.js
@@ -0,0 +1,17 @@
+function loadingBar(percentage){
+
+   const percentageNumber = percentage / 10;
+   const bar = "%".repeat(percentageNumber) + ".".repeat(10 - percentageNumber);
+
+    if(percentage === 100){
+        console.log(`100% Complete`);
+        console.log(`[${bar}]`);
+    }
+    else {
+        console.log(`${percentage}% [${bar}]`);
+        console.log(`Still loading...`);
+    }
+}
+
+
+loadingBar(30)
