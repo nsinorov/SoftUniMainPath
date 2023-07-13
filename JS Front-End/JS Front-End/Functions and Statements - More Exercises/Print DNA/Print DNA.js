d7764bb59170af7ed8a6b14f157b8b9dc04aa502
Nikolay Sinorov <nsinorov123@gmail.com>
2023-07-13 23:20:09 +0300

    Add files via upload

---
+++ JS Front-End/JS Front-End/Functions and Statements - More Exercises/Print DNA/Print DNA.js
@@ -0,0 +1,32 @@
+function printDNA(rows){
+
+    const pattern = [
+        ['A', 'T'],
+        ['C', 'G'],
+        ['T', 'T'],
+        ['A', 'G'], 
+        ['G', 'G']
+    ];
+
+    for (let index = 0; index < rows; index++) {
+      
+        const [x, y] = pattern[index % pattern.length];
+    
+        if(index % 4 === 0){
+            console.log(`**${x}${y}**`);
+            continue;
+        }
+
+        if(index % 2 === 1){
+            console.log(`*${x}--${y}*`);
+            continue;
+        }
+
+        if(index % 2 === 0){
+            console.log(`${x}----${y}`);
+            continue;
+        }
+    }
+}
+ 
+printDNA(10)
