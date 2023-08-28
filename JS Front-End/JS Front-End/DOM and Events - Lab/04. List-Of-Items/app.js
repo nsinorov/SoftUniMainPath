82a13944acd7cc0480a3ef08feede87eed5ea0dc
Nikolay Sinorov <nsinorov123@gmail.com>
2023-08-28 18:28:49 +0300

    Add files via upload

---
+++ JS Front-End/JS Front-End/DOM and Events - Lab/04. List-Of-Items/app.js
@@ -0,0 +1,7 @@
+function addItem() {
+    const value = document.querySelector("#newItemText").value;
+  
+    const item = document.createElement("li");
+    item.textContent = value;
+    document.querySelector("ul").appendChild(item);
+  }
