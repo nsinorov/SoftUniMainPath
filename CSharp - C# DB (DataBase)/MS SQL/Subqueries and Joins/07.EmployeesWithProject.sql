c047bf1ec06c12d082c607256287203b07bf5da2
Nikolay Sinorov <nsinorov123@gmail.com>
2023-10-10 22:26:25 +0300

    Add files via upload

---
+++ CSharp - C# DB (DataBase)/MS SQL/Subqueries and Joins/07.EmployeesWithProject.sql
@@ -0,0 +1,11 @@
+SELECT TOP(5)
+	  e.EmployeeID 
+	 ,e.FirstName
+	 ,p.[Name] AS [Name]
+FROM Employees AS e
+JOIN EmployeesProjects AS ep
+	ON e.EmployeeID = ep.EmployeeID
+JOIN Projects AS p
+    ON ep.ProjectID = p.ProjectID
+WHERE p.StartDate > 13/08/2002 AND p.EndDate IS NULL
+ORDER BY e.EmployeeID
