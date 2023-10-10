54409cab6a1f8f126867c14ff15d20d470b15bb9
Nikolay Sinorov <nsinorov123@gmail.com>
2023-10-10 22:26:04 +0300

    Add files via upload

---
+++ CSharp - C# DB (DataBase)/MS SQL/Subqueries and Joins/05.EmployeesWithoutProjects.sql
@@ -0,0 +1,8 @@
+SELECT TOP(3) 
+	   e.EmployeeID, 
+	   e.FirstName
+FROM Employees AS e
+LEFT JOIN EmployeesProjects AS ep
+	ON e.EmployeeID = ep.EmployeeID
+WHERE ep.ProjectID IS NULL
+ORDER BY e.[EmployeeID]
