61e98753cd3bfedbd16aed64a4097b25f95e68f5
Nikolay Sinorov <nsinorov123@gmail.com>
2023-10-10 21:25:03 +0300

    Add files via upload

---
+++ CSharp - C# DB (DataBase)/MS SQL/Built-in Functions/13.MixOfPeakAndRiverNames.sql
@@ -0,0 +1,5 @@
+SELECT PeakName, RiverName,
+LOWER(CONCAT(SUBSTRING(PeakName, 1, LEN(PeakName)-1), SUBSTRING(RiverName, 1, LEN(RiverName)))) AS [Mix]
+FROM Peaks, Rivers
+WHERE RIGHT(PeakName, 1) = LEFT(RiverName, 1)
+ORDER BY Mix
