d29f9faf3d7b366fd87ea2b399d2285696cf25a9
Nikolay Sinorov <nsinorov123@gmail.com>
2023-10-23 23:26:12 +0300

    Add files via upload

---
+++ CSharp - C# DB (DataBase)/Entity Framework Core/ADO.NET - Lab/AdoDotNetDemo/AdoDotNetDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+
+string connectionString = "Server=.;Database=;User Id=;Password=";
+
+using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+{
+    sqlConnection.Open();
+
+    string query = "SELECT COUNT(*) FROM Employees";
+
+    using (SqlCommand sqlCommand = new SqlCommand(query, sqlConnection))
+    {
+        int count = (int)sqlCommand.ExecuteScalar();
+
+        Console.WriteLine($"Employees count: {count}");
+    }
+}
+
+// Препоръчително е да използваме Async, по-добре е за сървърите
