f0919b7bb3ddcedd0fe6326afb1f06073ab1de40
Nikolay Sinorov <nsinorov123@gmail.com>
2023-10-26 23:07:39 +0300

    Add files via upload

---
+++ CSharp - C# DB (DataBase)/Entity Framework Core/ADO.NET - Exercise/Villain Names/Villain Names/SQLQueries.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villain_Names
+{
+    public static class SQLQueries
+    {
+        public const string GetVillainsWithNumberOfMinions = @"SELECT [Name],	COUNT(*) [TotalMinions]
+                                                        FROM Villains v
+                                                        JOIN MinionsVillains mv ON mv.VillainId = v.Id
+                                                        GROUP BY [Name]
+                                                        HAVING COUNT(*) > 3
+                                                        ORDER BY TotalMinions
+                                                        ";
+    }
+}
