aaabada4ef5f62db4b6ad5203eadb40a1053bb57
Nikolay Sinorov <nsinorov123@gmail.com>
2023-10-27 12:42:02 +0300

    Add files via upload

---
+++ CSharp - C# DB (DataBase)/Entity Framework Core/ADO.NET - Exercise/Initial Setup/Initial Setup/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.SqlClient;
+
+SqlConnection connection = new SqlConnection("Server=.;Integrated Security=true");
+connection.Open();
+
+using (connection)
+{
+    CreateDB(connection);
+    CreateDbTables(connection);
+}
+
+static void CreateDbTables(SqlConnection connection)
+{
+    string query = File.ReadAllText(@"../../CreateMinionsDbTables.sql");
+    SqlCommand command = new SqlCommand(query, connection);
+    Console.WriteLine("Created DB tables. Rows affected {0}.", command.ExecuteNonQuery());
+}
+
+static void CreateDB(SqlConnection connection)
+{
+    string query = "CREATE DATABASE MinionsDB";
+    SqlCommand command = new SqlCommand(query, connection);
+    command.ExecuteNonQuery();
+    Console.WriteLine("Created database MinionsDB.");
+}
