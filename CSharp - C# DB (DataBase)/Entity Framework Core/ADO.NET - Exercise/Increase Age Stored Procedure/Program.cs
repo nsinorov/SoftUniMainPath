abde2181126425c5c21932f064b3cd397ccb95c7
Nikolay Sinorov <nsinorov123@gmail.com>
2023-10-31 23:06:59 +0200

    Add files via upload

---
+++ CSharp - C# DB (DataBase)/Entity Framework Core/ADO.NET - Exercise/Increase Age Stored Procedure/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Data.SqlClient;
+
+SqlConnection connection = new SqlConnection("Server=.;Database=MinionsDB;Integrated security=true");
+connection.Open();
+
+using (connection)
+{
+    Console.Write("Enter minion ID: ");
+    int minionId = int.Parse(Console.ReadLine());
+
+    IncreaseAgeWithProc(connection, minionId);
+    PrintMinionsNameAge(connection, minionId);
+}
+
+static void PrintMinionsNameAge(SqlConnection connection, int minionId)
+{
+    string query = @"SELECT Name, Age FROM Minions WHERE Id = @id";
+    SqlCommand command = new SqlCommand(query, connection);
+    command.Parameters.AddWithValue("@id", minionId);
+    SqlDataReader reader = command.ExecuteReader();
+
+    using (reader)
+    {
+        while (reader.Read())
+        {
+            string name = (string)reader["Name"];
+            int age = (int)reader["Age"];
+            Console.WriteLine($"{name} {age}");
+        }
+    }
+}
+
+static void IncreaseAgeWithProc(SqlConnection connection, int minionId)
+{
+    string execProc = @"EXEC usp_GetOlder @id";
+    SqlCommand command = new SqlCommand(execProc, connection);
+    command.Parameters.AddWithValue("@id", minionId);
+    command.ExecuteNonQuery();
+}
---
+++ CSharp - C# DB (DataBase)/Entity Framework Core/ADO.NET - Exercise/Increase Age Stored Procedure/usp_GetOlder.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE usp_GetOlder (@id int)
+AS
+BEGIN
+  UPDATE Minions
+  SET Age += 1
+  WHERE Id = @id
+END
