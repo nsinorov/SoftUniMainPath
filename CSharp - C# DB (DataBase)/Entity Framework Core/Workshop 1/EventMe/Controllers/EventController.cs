578b9998ca298f2a6f2a5fca85727b6678c33e41
Nikolay Sinorov <nsinorov123@gmail.com>
2023-11-29 17:50:40 +0200

    Add files via upload

---
+++ CSharp - C# DB (DataBase)/Entity Framework Core/Workshop 1/EventMe/Controllers/EventController.cs
@@ -0,0 +1,129 @@
+﻿using EventMe.Core.Constants;
+using EventMe.Core.Contracts;
+using EventMe.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace EventMe.Controllers
+{
+    public class EventController : Controller
+    {
+        private readonly IEventService eventService;
+
+        private readonly ILogger logger;
+
+        public EventController(
+            IEventService _eventService,
+            ILogger<EventController> _logger)
+        {
+            eventService = _eventService;
+            logger = _logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = await eventService.GetAllAsync();
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            var model = new EventModel();
+            ViewBag.Towns = await GetTownsAsync();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(EventModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await eventService.CreateAsync(model);
+
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (ArgumentException ex)
+                {
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "Error creating event");
+                    ModelState.AddModelError(string.Empty, UserMessageConstants.UnknownError);
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var model = await eventService.GetByIdAsync(id);
+            ViewBag.Towns = await GetTownsAsync();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EventModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await eventService.EditAsync(model);
+
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (ArgumentException ex)
+                {
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "Error editing event");
+                    ModelState.AddModelError(string.Empty, UserMessageConstants.UnknownError);
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await eventService.DeleteAsync(id);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Delete failed");
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            var model = await eventService.GetByIdAsync(id);
+
+            return View(model);
+        }
+
+        private async Task<IEnumerable<SelectListItem>> GetTownsAsync()
+        {
+            var towns = await eventService.GetTownsAsync();
+
+            return towns
+                .Select(t => new SelectListItem(t.Name, t.Id.ToString()));
+        }
+    }
+}
---
+++ CSharp - C# DB (DataBase)/Entity Framework Core/Workshop 1/EventMe/Extensions/EventMeServiceCollectionExtension.cs
@@ -0,0 +1,28 @@
+﻿using EventMe.Core.Contracts;
+using EventMe.Core.Services;
+using EventMe.Infrastructure.Data;
+using EventMe.Infrastructure.Data.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class EventMeServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IEventService, EventService>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<EventMeDbContext>(options =>
+                           options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IRepository, Repository>();
+
+            return services;
+        }
+    }
+}
