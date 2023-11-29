b0e226d023305ad850a70b20339ee47525f90b46
Nikolay Sinorov <nsinorov123@gmail.com>
2023-11-29 17:51:24 +0200

    Add files via upload

---
+++ CSharp - C# DB (DataBase)/Entity Framework Core/Workshop 2/Resources/ApplicationUserController.cs
@@ -0,0 +1,115 @@
+﻿using Blog.Infrastructure.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Blog.Core.Models.User;
+
+namespace Blog.Controllers
+{
+    [Authorize]
+    public class ApplicationUserController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public ApplicationUserController(
+            UserManager<ApplicationUser> _userManager,
+            SignInManager<ApplicationUser> _signInManager)
+        {
+            userManager = _userManager;
+            signInManager = _signInManager;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("All", "Article");
+            }
+
+            var model = new RegisterViewModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = new ApplicationUser()
+            {
+                Email = model.Email,
+                UserName = model.UserName
+            };
+
+            var result = await userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Login", "ApplicationUser");
+            }
+
+            foreach (var item in result.Errors)
+            {
+                ModelState.AddModelError("", item.Description);
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("All", "Article");
+            }
+
+            var model = new LoginViewModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = await userManager.FindByNameAsync(model.UserName);
+
+            if (user != null)
+            {
+                var result = await signInManager.PasswordSignInAsync(user, model.Password, false, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("All", "Article");
+                }
+            }
+
+            ModelState.AddModelError("", "Invalid login");
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
