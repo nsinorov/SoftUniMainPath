a9f247ca7d336947adccf06602d970e675d8f946
Nikolay Sinorov <nsinorov123@gmail.com>
2023-11-29 17:27:31 +0200

    Add files via upload

---
+++ CSharp - C# DB (DataBase)/Entity Framework Core/Best Practices And Architecture - Exercise/PetStore/PetStore.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PetStore.Web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
