eb4e8276d0e67849fa0d249c1de94cf270ab160a
Nikolay Sinorov <nsinorov123@gmail.com>
2024-02-02 18:57:19 +0200

    Add files via upload

---
+++ C# Web/ASP.NET Fundamentals/04. ASP.NET Core Introduction - Exercise/MVCIntroDemo/MVCIntroDemo/Controllers/NumbersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCIntroDemo.Controllers
+{
+    public class NumbersController : Controller
+    {
+        // Dependancy Injection -------------------------------------------------
+        private readonly ILogger<NumbersController> _logger;
+
+        public NumbersController(ILogger<NumbersController> logger)
+        {
+            _logger = logger;
+        }
+
+        // -----------------------------------------------------------------------
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
