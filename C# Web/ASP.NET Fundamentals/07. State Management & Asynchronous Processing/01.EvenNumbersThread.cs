d7fec5e2a95e13f96a6bd14b8b01047c5b29471a
Nikolay Sinorov <nsinorov123@gmail.com>
2024-03-05 18:02:11 +0200

    Update 01.EvenNumbersThread.cs

--- C# Web/ASP.NET Fundamentals/07. State Management & Asynchronous Processing/01.EvenNumbersThread.cs
+++ C# Web/ASP.NET Fundamentals/07. State Management & Asynchronous Processing/01.EvenNumbersThread.cs
@@ -1,1 +1,17 @@
+﻿//Reading data
+int start = int.Parse(Console.ReadLine());
+int end = int.Parse(Console.ReadLine());
 
+Thread evens = new Thread(() => PrintEvenNumbers(start, end));
+
+evens.Start();
+evens.Join();
+Console.WriteLine("Thread Finished Work");
+
+static void PrintEvenNumbers(int start, int end)
+{
+	for (int i = start; i <= end; i++)
+	{
+		Console.WriteLine(i);
+	}
+}
