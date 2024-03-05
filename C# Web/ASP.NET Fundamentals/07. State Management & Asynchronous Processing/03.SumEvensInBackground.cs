64e8d04055e60c0972b9e9f63694f09ecb22e627
Nikolay Sinorov <nsinorov123@gmail.com>
2024-03-05 18:03:00 +0200

    Update 03.SumEvensInBackground.cs

--- C# Web/ASP.NET Fundamentals/07. State Management & Asynchronous Processing/03.SumEvensInBackground.cs
+++ C# Web/ASP.NET Fundamentals/07. State Management & Asynchronous Processing/03.SumEvensInBackground.cs
@@ -1,1 +1,26 @@
+﻿long sum = 0;
 
+var task = Task.Run(() =>
+{
+	for (long i = 0; i < 1000000000; i++)
+	{
+		if (i % 2 == 0)
+		{
+			sum += i;
+		}
+	}
+});
+
+while (true)
+{
+	var line = Console.ReadLine();
+
+	if (line == "exit")
+	{
+
+	}
+	else if (line == "show")
+    {
+		Console.WriteLine(sum);
+    }
+}
