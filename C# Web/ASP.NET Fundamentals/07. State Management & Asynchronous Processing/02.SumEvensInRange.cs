6020c1b40fb6310376d1290a386d6375c414ff7d
Nikolay Sinorov <nsinorov123@gmail.com>
2024-03-05 18:02:35 +0200

    Update 02.SumEvensInRange.cs

--- C# Web/ASP.NET Fundamentals/07. State Management & Asynchronous Processing/02.SumEvensInRange.cs
+++ C# Web/ASP.NET Fundamentals/07. State Management & Asynchronous Processing/02.SumEvensInRange.cs
@@ -1,1 +1,30 @@
+﻿using System.Runtime.CompilerServices;
 
+while (true)
+{
+    string command = Console.ReadLine();
+
+	if (command == "show")
+	{
+		var result = SumAsync();
+		Console.WriteLine(result);
+	}
+}
+
+static long SumAsync()
+{
+	return Task.Run(() =>
+	{
+		long sum = 0;
+
+		for (int i = 1; i < 10000; i++)
+		{
+			if (i % 2 == 0)
+			{
+				sum += i;
+			}
+		}
+
+		return sum;
+	}).Result;
+}
